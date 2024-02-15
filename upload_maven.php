24972795d993651d85f608c648880c0753273520
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-15 16:33:32 +0900

    php

--- upload_maven.php
+++ upload_maven.php
@@ -1,23 +1,30 @@
 <?php
-define('VERSION', '1.1.5');
+$info_properties = file_get_contents('../info.properties');
+preg_match('/mod_version=(.*)/', $info_properties, $matches);
+$matches[1] = str_replace(["\r", "\n"], '', $matches[1]);
+
+define('VERSION', $matches[1]);
 
 define('DIRS', array(
-	'common' => './common/build/libs/',
-	'fabric' => './fabric/build/libs/',
-	'forge' => './forge/build/libs/',
+	'common' => 'common/build/',
+	'fabric' => 'fabric/build/',
+	'forge' => 'forge/build/',
+	'neoforge' => 'neoforge/build/',
 ));
 
 define('GAME_VERSIONS', array(
-	'1.16.5' => '1.16', 
-	'1.17' => '1.17', 
-	'1.18' => '1.18', 
-	'1.19' => '1.19',
+	'1.16.5' => '1.16.5',
+	'1.18.2' => '1.18.2',
+	'1.19.2' => '1.19.2',
+	'1.20.1' => '1.20.1',
+	'1.20.4' => '1.20.4',
 ));
 
 define('PLATFORM_FILE_MARK', array(
 	'common' => '',
 	'fabric' => '-fabric',
 	'forge' => '-forge',
+	'neoforge' => '-neoforge',
 ));
 
 foreach (DIRS as $type => $dir) {
@@ -26,23 +33,42 @@ foreach (DIRS as $type => $dir) {
 		
 		$postData = array();
 		
-		$postData['group_id'] = 'ml.pkom';
-		$postData['artifact_id'] = 'mcpitanlibarch-' . $type . '+' . $ver;
+		$postData['group_id'] = 'net.pitan76';
+		$postData['artifact_id'] = 'mcpitanlib-' . $type . '+' . $ver;
 		$postData['version'] = VERSION;
-		
+
 		$files = array(
-			$dir . 'mcpitanlibarch-' . VERSION . '-' . $file_ver . PLATFORM_FILE_MARK[$type] . '.jar',
-			$dir . 'mcpitanlibarch-' . VERSION . '-' . $file_ver . PLATFORM_FILE_MARK[$type] . '-sources.jar',
+			$dir . 'libs/mcpitanlib-' . VERSION . '-' . $file_ver . PLATFORM_FILE_MARK[$type] . '.jar',
+			$dir . 'libs/mcpitanlib-' . VERSION . '-' . $file_ver . PLATFORM_FILE_MARK[$type] . '-sources.jar',
+			$dir . 'publications/maven' .  ucfirst($type) . '/mcpitanlib-' . $file_ver . '.pom',
 		);
-		
+
+		$pom = '../' . $dir . 'publications/maven' .  ucfirst($type) . '/mcpitanlib-' . $file_ver . '.pom';
+
+		$pom_str = file_get_contents($pom);
+		$pom_str = preg_replace('/' . preg_quote(VERSION . '-' . $file_ver . PLATFORM_FILE_MARK[$type] . '</version>', '/'). '/', VERSION . '</version>', $pom_str, 1);
+		$pom_str = preg_replace('/' . preg_quote('mcpitanlib' . PLATFORM_FILE_MARK[$type]  . '</artifactId>', '/') . '/', 'mcpitanlib' .  ($type == "common" ? '-common' : PLATFORM_FILE_MARK[$type]) . '+' . $file_ver . '</artifactId>', $pom_str, 1);
+
+		file_put_contents($pom, $pom_str);
+
+		$notfound = false;
+
 		foreach ($files as $index => $file) {
+
+			if (!file_exists(realpath("../" . $file))) {
+				echo "not found" . $file;
+				$notfound = true;
+				continue;
+			}
 			$postData['upload[' . $index . ']'] = curl_file_create(
-				realpath($file),
-				mime_content_type($file),
-				basename($file)
+				realpath("../" . $file),
+				mime_content_type("../" . $file),
+				basename("../" . $file)
 			);
 			echo "Uploading '" . $file . "'\n";
 		}
+
+		if ($notfound) continue;
 		
 		$request = curl_init('http://localhost/maven/maven.php');
 		curl_setopt($request, CURLOPT_POST, true);
@@ -56,5 +82,3 @@ foreach (DIRS as $type => $dir) {
 		curl_close($request);
 	}
 }
-
-
