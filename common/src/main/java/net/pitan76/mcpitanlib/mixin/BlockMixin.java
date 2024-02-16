f51c762dad0df336c1c14f8941b843f87b3a81b3
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-02-16 18:36:47 +0900

    Add new Event APIs

--- common/src/main/java/net/pitan76/mcpitanlib/mixin/BlockMixin.java
+++ common/src/main/java/net/pitan76/mcpitanlib/mixin/BlockMixin.java
@@ -12,8 +12,9 @@ import net.pitan76.mcpitanlib.api.block.ExtendBlockProvider.Options;
 import net.pitan76.mcpitanlib.api.event.block.BlockBreakEvent;
 import net.pitan76.mcpitanlib.api.event.block.BlockPlacedEvent;
 import net.pitan76.mcpitanlib.api.event.block.result.BlockBreakResult;
-import net.pitan76.mcpitanlib.api.event.v1.BlockEventRegistry;
 import net.pitan76.mcpitanlib.api.event.v1.listener.BlockBreakTask;
+import net.pitan76.mcpitanlib.api.event.v1.listener.BlockPlacedTask;
+import net.pitan76.mcpitanlib.api.event.v2.BlockEventRegistry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,10 +25,17 @@ public class BlockMixin {
 
     @Inject(method = "onPlaced", at = @At("HEAD"), cancellable = true)
     private void inject_onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack, CallbackInfo ci) {
-        if (!BlockEventRegistry.onPlacedListeners.isEmpty()) {
-            BlockEventRegistry.onPlacedListeners.forEach(l -> l.onPlaced(new BlockPlacedEvent(world, pos, state, placer, stack)));
+        // イベントを呼び出す
+        if (!BlockEventRegistry.ON_PLACED.isEmpty()) {
+            int maxPriority = BlockEventRegistry.ON_PLACED.getMaxPriority();
+            for (int p = maxPriority; p >= 0; p--) {
+                for (BlockPlacedTask listener : BlockEventRegistry.ON_PLACED.getListenersAsList(p)) {
+                    listener.onPlaced(new BlockPlacedEvent(world, pos, state, placer, stack));
+                }
+            }
         }
 
+        // ExtendBlockProviderを実装している場合
         if (this instanceof ExtendBlockProvider) {
             ExtendBlockProvider provider = (ExtendBlockProvider) this;
             Options options = new Options();
@@ -38,13 +46,18 @@ public class BlockMixin {
     }
 
     @Inject(method = "onBreak", at = @At("HEAD"), cancellable = true)
-    private void inject_onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo ci) {
-        if (!BlockEventRegistry.onBreakListeners.isEmpty()) {
-            for (BlockBreakTask l : BlockEventRegistry.onBreakListeners) {
-                l.onBreak(new BlockBreakEvent(world, pos, state, player));
+    private void inject_onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfoReturnable<BlockState> cir) {
+        // イベントを呼び出す
+        if (!BlockEventRegistry.ON_BREAK.isEmpty()) {
+            int maxPriority = BlockEventRegistry.ON_BREAK.getMaxPriority();
+            for (int p = maxPriority; p >= 0; p--) {
+                for (BlockBreakTask listener : BlockEventRegistry.ON_BREAK.getListenersAsList(p)) {
+                    listener.onBreak(new BlockBreakEvent(world, pos, newState, player));
+                }
             }
         }
 
+        // ExtendBlockProviderを実装している場合
         if (this instanceof ExtendBlockProvider) {
             ExtendBlockProvider provider = (ExtendBlockProvider) this;
             Options options = new Options();
