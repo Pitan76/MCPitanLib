777526d2212273e98b9c45624520ea86e89f4465
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-17 22:57:57 +0900

    fix apis

--- common/src/main/java/net/pitan76/mcpitanlib/api/event/item/CraftEvent.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/event/item/CraftEvent.java
@@ -1,12 +1,28 @@
 package net.pitan76.mcpitanlib.api.event.item;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.pitan76.mcpitanlib.api.entity.Player;
+import org.jetbrains.annotations.Nullable;
 
 public class CraftEvent {
 
     public ItemStack stack;
     public World world;
+    public Player player;
+
+    public CraftEvent(ItemStack stack, World world, Player player) {
+        this.stack = stack;
+        this.world = world;
+        this.player = player;
+    }
+
+    public CraftEvent(ItemStack stack, World world, PlayerEntity player) {
+        this.stack = stack;
+        this.world = world;
+        this.player = new Player(player);
+    }
 
     public CraftEvent(ItemStack stack, World world) {
         this.stack = stack;
@@ -19,6 +35,11 @@ public class CraftEvent {
 
     public World getWorld() {
         return world;
+    }
+
+    @Nullable
+    public Player getPlayer() {
+        return player;
     }
 
     public boolean isClient() {
