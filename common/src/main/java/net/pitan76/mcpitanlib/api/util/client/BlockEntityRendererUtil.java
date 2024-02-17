b9d0596399d0b696b5c39476c84c803b10ece0b8
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-17 22:15:24 +0900

    1165

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/client/BlockEntityRendererUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/client/BlockEntityRendererUtil.java
@@ -1,14 +1,5 @@
 package net.pitan76.mcpitanlib.api.util.client;
 
-import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
-import net.pitan76.mcpitanlib.api.client.registry.CompatRegistryClient;
-
 public class BlockEntityRendererUtil {
-    public static BlockEntityRendererFactory.Context convert(CompatRegistryClient.BlockEntityRendererFactory.Context ctx) {
-        return new BlockEntityRendererFactory.Context(ctx.getRenderDispatcher(), ctx.getRenderManager(), ctx.getLayerRenderDispatcher(), ctx.getTextRenderer());
-    }
-
-    public static CompatRegistryClient.BlockEntityRendererFactory.Context convert(BlockEntityRendererFactory.Context ctx) {
-        return new CompatRegistryClient.BlockEntityRendererFactory.Context(ctx.getRenderDispatcher(), ctx.getRenderManager(), null, null, ctx.getLayerRenderDispatcher(), ctx.getTextRenderer());
-    }
+    // Developing...
 }
