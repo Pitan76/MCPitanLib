05660420aaa911db06041f5396ec9bd71ca05d14
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-17 22:00:51 +0900

    add APIs

---
+++ common/src/main/java/net/pitan76/mcpitanlib/api/block/CompatChestBlock.java
@@ -0,0 +1,33 @@
+package net.pitan76.mcpitanlib.api.block;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ChestBlock;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.util.math.BlockPos;
+import net.pitan76.mcpitanlib.api.event.block.TileCreateEvent;
+
+import java.util.function.Supplier;
+
+public class CompatChestBlock extends ChestBlock {
+    public CompatChestBlock(Settings settings, Supplier<BlockEntityType<? extends ChestBlockEntity>> supplier) {
+        super(settings, supplier);
+    }
+
+    /**
+     * @deprecated Use {@link #createBlockEntity(TileCreateEvent)} instead.
+     */
+    @Deprecated
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return createBlockEntity(new TileCreateEvent(pos, state));
+    }
+
+    /**
+     * @see ExtendBlockEntityProvider#createBlockEntity(TileCreateEvent)
+     */
+    public BlockEntity createBlockEntity(TileCreateEvent event) {
+        return super.createBlockEntity(event.getBlockPos(), event.getBlockState());
+    }
+}
