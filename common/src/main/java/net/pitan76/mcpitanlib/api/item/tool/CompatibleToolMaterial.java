7486685f7336d1d4a863df0c6f910ff44525a43e
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-02-19 17:16:02 +0900

    getCompatMiningLevel

--- common/src/main/java/net/pitan76/mcpitanlib/api/item/tool/CompatibleToolMaterial.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/item/tool/CompatibleToolMaterial.java
@@ -1,7 +1,29 @@
 package net.pitan76.mcpitanlib.api.item.tool;
 
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
 
 public interface CompatibleToolMaterial extends ToolMaterial {
+    @Deprecated
+    @Override
+    default int getMiningLevel() {
+        return getCompatMiningLevel();
+    }
 
+    int getCompatMiningLevel();
+
+    @Override
+    float getAttackDamage();
+
+    @Override
+    float getMiningSpeedMultiplier();
+
+    @Override
+    Ingredient getRepairIngredient();
+
+    @Override
+    int getDurability();
+
+    @Override
+    int getEnchantability();
 }
