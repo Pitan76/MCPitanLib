a8f406133e720ed5d0c971107683998c81c92493
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-03-04 21:55:03 +0900

    fix apis2

--- common/src/main/java/net/pitan76/mcpitanlib/api/client/gui/widget/SimpleListWidget.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/gui/widget/SimpleListWidget.java
@@ -94,7 +94,7 @@ public class SimpleListWidget extends ElementListWidget<SimpleListWidget.WidgetE
             widget.y = y;
             if (widget instanceof SimpleSliderWidget) {
                 SimpleSliderWidget slider = (SimpleSliderWidget) widget;
-                if (y + slider.getHeight() > slider.listWidget.getY() + slider.listWidget.getHeight())
+                if (y + slider.getHeight() > slider.listWidget.top + slider.listWidget.getHeight())
                     return;
 
             }
