24d4d80dfc5718027e8c8bc9d17f64dee03eb47e
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-03-04 16:05:38 +0900

    update loom

--- common/src/main/java/net/pitan76/mcpitanlib/api/client/SimpleHandledScreen.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/SimpleHandledScreen.java
@@ -36,6 +36,10 @@ public abstract class SimpleHandledScreen extends HandledScreen<ScreenHandler> {
         // addButton
     }
 
+    public <T extends Element & Selectable> T addSelectableChild_compatibility(T selectableElement) {
+        return super.addSelectableChild(selectableElement);
+    }
+
     public CompatibleTexturedButtonWidget addDrawableCTBW(CompatibleTexturedButtonWidget widget) {
         return addDrawableChild_compatibility(widget);
     }
