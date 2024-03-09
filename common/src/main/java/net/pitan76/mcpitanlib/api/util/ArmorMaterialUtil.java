9e52b957992cc6d761860a6f195cb311d1566197
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-03-10 01:11:55 +0900

    add fix API (all) 2

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/ArmorMaterialUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/ArmorMaterialUtil.java
@@ -2,11 +2,19 @@ package net.pitan76.mcpitanlib.api.util;
 
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
 import net.pitan76.mcpitanlib.api.item.ArmorEquipmentType;
 import net.pitan76.mcpitanlib.api.item.CompatibleArmorMaterial;
 
 public class ArmorMaterialUtil {
     public static CompatibleArmorMaterial create(String name, int[] durability, int[] protectionAmounts, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Ingredient repairIngredient) {
+        return create(new Identifier(name), durability, protectionAmounts, enchantability, equipSound, toughness, knockbackResistance, repairIngredient);
+    }
+    public static CompatibleArmorMaterial create(String name, int durability, int protectionAmount, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Ingredient repairIngredient) {
+        return create(new Identifier(name), durability, protectionAmount, enchantability, equipSound, toughness, knockbackResistance, repairIngredient);
+    }
+
+    public static CompatibleArmorMaterial create(Identifier id, int[] durability, int[] protectionAmounts, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Ingredient repairIngredient) {
         return new CompatibleArmorMaterial() {
             @Override
             public int getDurability(ArmorEquipmentType type) {
@@ -66,8 +74,8 @@ public class ArmorMaterialUtil {
             }
 
             @Override
-            public String getName() {
-                return name;
+            public Identifier getId() {
+                return id;
             }
 
             @Override
@@ -82,7 +90,7 @@ public class ArmorMaterialUtil {
         };
     }
 
-    public static CompatibleArmorMaterial create(String name, int durability, int protectionAmount, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Ingredient repairIngredient) {
+    public static CompatibleArmorMaterial create(Identifier id, int durability, int protectionAmount, int enchantability, SoundEvent equipSound, float toughness, float knockbackResistance, Ingredient repairIngredient) {
         return new CompatibleArmorMaterial() {
             @Override
             public int getDurability(ArmorEquipmentType type) {
@@ -110,8 +118,8 @@ public class ArmorMaterialUtil {
             }
 
             @Override
-            public String getName() {
-                return name;
+            public Identifier getId() {
+                return id;
             }
 
             @Override
