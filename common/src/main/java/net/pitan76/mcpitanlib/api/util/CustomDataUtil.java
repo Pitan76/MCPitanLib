2aeead48bd34c6d990bf31d3b8922af04821ff67
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-03-10 15:49:36 +0900

    add APIs 3

---
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/CustomDataUtil.java
@@ -0,0 +1,132 @@
+package net.pitan76.mcpitanlib.api.util;
+
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.NbtComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+
+import java.util.Set;
+
+/**
+ * カスタムデータのユーティリティクラス
+ */
+public class CustomDataUtil {
+    /**
+     * NBTを取得する。存在しない場合は新しいNBTを作成する。
+     * @param stack ItemStack
+     * @return NBT
+     */
+    public static NbtCompound getOrCreateNbt(ItemStack stack) {
+        if (!hasNbt(stack)) {
+            return NbtUtil.create();
+        }
+
+        return getNbt(stack);
+    }
+
+    /**
+     * NBTを設定する。
+     * @param stack ItemStack
+     * @param nbt NBT
+     */
+    public static void setNbt(ItemStack stack, NbtCompound nbt) {
+        stack.set(DataComponentTypes.CUSTOM_DATA, NbtComponent.of(nbt));
+    }
+    
+    /**
+     * NBTが存在するかどうかを取得する。
+     * @param stack ItemStack
+     * @return NBTが存在するかどうか
+     */
+    public static boolean hasNbt(ItemStack stack) {
+        return stack.contains(DataComponentTypes.CUSTOM_DATA);
+    }
+    
+    /**
+     * NBTを取得する。
+     * @param stack ItemStack
+     * @return NBT
+     */
+    public static NbtCompound getNbt(ItemStack stack) {
+        return stack.get(DataComponentTypes.CUSTOM_DATA).copyNbt();
+    }
+    
+    /**
+     * 値を設定する。
+     * @param stack ItemStack
+     * @param key キー
+     * @param value 値
+     */
+    public static void put(ItemStack stack, String key, NbtCompound value) {
+        NbtCompound nbt = getOrCreateNbt(stack);
+        NbtUtil.put(nbt, key, value);
+        setNbt(stack, nbt);
+    }
+    
+    /**
+     * 値を取得する。
+     * @param stack ItemStack
+     * @param key キー
+     * @return 値
+     */
+    public static NbtCompound get(ItemStack stack, String key) {
+        NbtCompound nbt = getNbt(stack);
+        return nbt.getCompound(key);
+    }
+    
+    /**
+     * 値を削除する。
+     * @param stack ItemStack
+     * @param key キー
+     */
+    public static void remove(ItemStack stack, String key) {
+        NbtCompound nbt = getNbt(stack);
+        nbt.remove(key);
+        setNbt(stack, nbt);
+    }
+    
+    /**
+     * 値が存在するかどうかを取得する。
+     * @param stack ItemStack
+     * @param key キー
+     * @return 値が存在するかどうか
+     */
+    public static boolean has(ItemStack stack, String key) {
+        NbtCompound nbt = getNbt(stack);
+        return nbt.contains(key);
+    }
+    
+    /**
+     * 指定した型の値を取得する
+     * @param stack ItemStack
+     * @param key キー
+     * @param clazz クラス
+     * @param <T> 値
+     */
+    public static <T> T get(ItemStack stack, String key, Class<T> clazz) {
+        NbtCompound nbt = getNbt(stack);
+        return NbtUtil.get(nbt, key, clazz);
+    }
+    
+    /**
+     * 値を設定する。
+     * @param stack ItemStack
+     * @param key キー
+     * @param value 値
+     */
+    public static <T> void set(ItemStack stack, String key, T value) {
+        NbtCompound nbt = getOrCreateNbt(stack);
+        NbtUtil.set(nbt, key, value);
+        setNbt(stack, nbt);
+    }
+    
+    /**
+     * キーの一覧を取得する。
+     * @param stack ItemStack
+     * @return キーの一覧
+     */
+    public static Set<String> getKeys(ItemStack stack) {
+        NbtCompound nbt = getNbt(stack);
+        return NbtUtil.getKeys(nbt);
+    }
+}
