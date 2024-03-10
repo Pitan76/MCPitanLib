3666de5fb1bc16dbd09c25a72b8ff3480ed09f19
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-03-10 16:07:07 +0900

    fix 1204

--- common/src/main/java/net/pitan76/mcpitanlib/api/item/CompatibleArmorMaterial.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/item/CompatibleArmorMaterial.java
@@ -3,17 +3,8 @@ package net.pitan76.mcpitanlib.api.item;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.recipe.Ingredient;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Util;
-
-import java.util.EnumMap;
-import java.util.List;
-import java.util.function.Supplier;
 
 public interface CompatibleArmorMaterial {
     int getDurability(ArmorEquipmentType type);
@@ -43,38 +34,47 @@ public interface CompatibleArmorMaterial {
     float getKnockbackResistance();
 
     @Deprecated
-    default RegistryEntry<ArmorMaterial> build() {
-        if (CompatibleArmorItem.CACHE.containsKey(this)) {
-            return CompatibleArmorItem.CACHE.get(this);
-        }
+    default ArmorMaterial build() {
+        return new ArmorMaterial() {
+            @Override
+            public int getDurability(ArmorItem.Type type) {
+                return CompatibleArmorMaterial.this.getDurability(ArmorEquipmentType.of(type));
+            }
 
-        RegistryEntry<ArmorMaterial> entry = register(getId(), Util.make(new EnumMap<>(ArmorItem.Type.class), (map) -> {
-                    map.put(ArmorItem.Type.HELMET, this.getProtection(ArmorEquipmentType.HEAD));
-                    map.put(ArmorItem.Type.CHESTPLATE, this.getProtection(ArmorEquipmentType.CHEST));
-                    map.put(ArmorItem.Type.LEGGINGS, this.getProtection(ArmorEquipmentType.LEGS));
-                    map.put(ArmorItem.Type.BOOTS, this.getProtection(ArmorEquipmentType.FEET));
+            @Override
+            public int getProtection(ArmorItem.Type type) {
+                return CompatibleArmorMaterial.this.getProtection(ArmorEquipmentType.of(type));
+            }
 
-                    map.put(ArmorItem.Type.BODY, this.getProtection(ArmorEquipmentType.BODY));
-                }), getEnchantability(), SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, getToughness(), getKnockbackResistance(),
-                this::getRepairIngredient
-        );
-        CompatibleArmorItem.CACHE.put(this, entry);
-        return entry;
-    }
+            @Override
+            public int getEnchantability() {
+                return CompatibleArmorMaterial.this.getEnchantability();
+            }
 
-    private static RegistryEntry<ArmorMaterial> register(Identifier id, EnumMap<ArmorItem.Type, Integer> defense, int enchantability, RegistryEntry<SoundEvent> equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient) {
-        List<ArmorMaterial.Layer> list = List.of(new ArmorMaterial.Layer(id));
-        return register(id, defense, enchantability, equipSound, toughness, knockbackResistance, repairIngredient, list);
-    }
+            @Override
+            public SoundEvent getEquipSound() {
+                return CompatibleArmorMaterial.this.getEquipSound();
+            }
 
-    private static RegistryEntry<ArmorMaterial> register(Identifier id, EnumMap<ArmorItem.Type, Integer> defense, int enchantability, RegistryEntry<SoundEvent> equipSound, float toughness, float knockbackResistance, Supplier<Ingredient> repairIngredient, List<ArmorMaterial.Layer> layers) {
-        EnumMap<ArmorItem.Type, Integer> enumMap = new EnumMap<>(ArmorItem.Type.class);
-        ArmorItem.Type[] var9 = ArmorItem.Type.values();
+            @Override
+            public Ingredient getRepairIngredient() {
+                return CompatibleArmorMaterial.this.getRepairIngredient();
+            }
 
-        for (ArmorItem.Type type : var9) {
-            enumMap.put(type, defense.get(type));
-        }
+            @Override
+            public String getName() {
+                return CompatibleArmorMaterial.this.getName();
+            }
 
-        return Registry.registerReference(Registries.ARMOR_MATERIAL, id, new ArmorMaterial(enumMap, enchantability, equipSound, repairIngredient, layers, toughness, knockbackResistance));
+            @Override
+            public float getToughness() {
+                return CompatibleArmorMaterial.this.getToughness();
+            }
+
+            @Override
+            public float getKnockbackResistance() {
+                return CompatibleArmorMaterial.this.getKnockbackResistance();
+            }
+        };
     }
 }
