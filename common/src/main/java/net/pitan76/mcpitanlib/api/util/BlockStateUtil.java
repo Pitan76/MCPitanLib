7ad564d30458b7d92225cc78e338bfb2e9239f61
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-06-28 09:52:51 +0900

    add BlockState API 2.2.5

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/BlockStateUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/BlockStateUtil.java
@@ -3,6 +3,7 @@ package net.pitan76.mcpitanlib.api.util;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.state.StateManager;
 
 public class BlockStateUtil {
     public static Block getBlock(BlockState state) {
@@ -19,5 +20,13 @@ public class BlockStateUtil {
 
     public static BlockSoundGroup getSoundGroup(BlockState state) {
         return state.getSoundGroup();
+    }
+
+    public static BlockState getDefaultState(Block block) {
+        return block.getDefaultState();
+    }
+
+    public static StateManager<Block, BlockState> getStateManager(Block block) {
+        return block.getStateManager();
     }
 }
