50de649f40d348485d6aaa7edf27f8024fd13bf7
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-06-29 18:14:20 +0900

    fix 121

---
+++ common/src/main/java/net/pitan76/mcpitanlib/api/item/CompatItems.java
@@ -0,0 +1,18 @@
+package net.pitan76.mcpitanlib.api.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+
+public class CompatItems {
+    public Item SHORT_GRASS = Items.SHORT_GRASS;
+    public Item TALL_GRASS = Items.TALL_GRASS;
+    public Item TURTLE_SCUTE = Items.TURTLE_SCUTE;
+
+    // Compatibility
+    public Item GRASS = SHORT_GRASS;
+    public Item SCUTE = TURTLE_SCUTE;
+
+    public static boolean isExist(Item item) {
+        return item != null && item != Items.AIR;
+    }
+}
