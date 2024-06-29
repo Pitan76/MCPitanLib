1bf021cd8209992868b5e75c3a6438a676945a94
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-06-30 00:30:06 +0900

    fix CompatItems info

--- common/src/main/java/net/pitan76/mcpitanlib/api/item/CompatItems.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/item/CompatItems.java
@@ -4,13 +4,13 @@ import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 
 public class CompatItems {
-    public Item SHORT_GRASS = Items.SHORT_GRASS;
-    public Item TALL_GRASS = Items.TALL_GRASS;
-    public Item TURTLE_SCUTE = Items.SCUTE;
+    public static Item SHORT_GRASS = Items.SHORT_GRASS;
+    public static Item TALL_GRASS = Items.TALL_GRASS;
+    public static Item TURTLE_SCUTE = Items.SCUTE;
 
     // Compatibility
-    public Item GRASS = SHORT_GRASS;
-    public Item SCUTE = TURTLE_SCUTE;
+    public static Item GRASS = SHORT_GRASS;
+    public static Item SCUTE = TURTLE_SCUTE;
 
     public static boolean isExist(Item item) {
         return item != null && item != Items.AIR;
