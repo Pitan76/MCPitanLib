d447efb5d103323286cc7d5e16c75d263316cc4f
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-06-29 18:14:20 +0900

    fix 121

--- common/src/main/java/net/pitan76/mcpitanlib/api/world/CompatiblePersistentState.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/world/CompatiblePersistentState.java
@@ -1,6 +1,7 @@
 package net.pitan76.mcpitanlib.api.world;
 
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.world.PersistentState;
 
 public abstract class CompatiblePersistentState extends PersistentState {
@@ -15,4 +16,12 @@ public abstract class CompatiblePersistentState extends PersistentState {
 
     // 1.16
     public abstract void readNbt(NbtCompound tag);
+
+    @Deprecated
+    @Override
+    public NbtCompound writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup registryLookup) {
+        return writeNbt(nbt);
+    }
+
+    public abstract NbtCompound writeNbt(NbtCompound tag);
 }
