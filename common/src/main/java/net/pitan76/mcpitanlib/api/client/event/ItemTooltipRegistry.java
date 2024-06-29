4b9b3c46102f8c6d9a7c1d7bfb03a771601dd0bb
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-06-29 21:22:36 +0900

    use tooltipevent116

--- common/src/main/java/net/pitan76/mcpitanlib/api/client/event/ItemTooltipRegistry.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/event/ItemTooltipRegistry.java
@@ -1,10 +1,10 @@
 package net.pitan76.mcpitanlib.api.client.event;
 
-import dev.architectury.event.events.client.ClientTooltipEvent;
+import me.shedaniel.architectury.event.events.TooltipEvent;
 import net.pitan76.mcpitanlib.api.client.event.listener.ItemTooltipListener;
 
 public class ItemTooltipRegistry {
     public static void registerItemTooltip(ItemTooltipListener listener) {
-        ClientTooltipEvent.ITEM.register(listener::onTooltip);
+        TooltipEvent.ITEM.register(listener::onTooltip);
     }
 }
