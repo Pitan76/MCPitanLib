cb167bacbec44070a531e51115295ccbf0d614f8
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-07-07 14:43:39 +0900

    1182

--- common/src/main/java/net/pitan76/mcpitanlib/api/event/nbt/WriteNbtArgs.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/event/nbt/WriteNbtArgs.java
@@ -1,14 +1,8 @@
 package net.pitan76.mcpitanlib.api.event.nbt;
 
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.registry.RegistryWrapper;
 
 public class WriteNbtArgs extends NbtRWArgs {
-
-    @Deprecated
-    public WriteNbtArgs(NbtCompound nbt, RegistryWrapper.WrapperLookup wrapperLookup) {
-        super(nbt, wrapperLookup);
-    }
 
     public WriteNbtArgs(NbtCompound nbt) {
         super(nbt);
