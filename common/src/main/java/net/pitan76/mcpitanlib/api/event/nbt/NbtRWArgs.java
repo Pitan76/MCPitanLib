6ec35b3d27f5ff8cd47f642b302e0c2d791c7034
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-07-08 21:03:01 +0900

    nbtargs

--- common/src/main/java/net/pitan76/mcpitanlib/api/event/nbt/NbtRWArgs.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/event/nbt/NbtRWArgs.java
@@ -1,28 +1,15 @@
 package net.pitan76.mcpitanlib.api.event.nbt;
 
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.registry.RegistryWrapper;
 
 public class NbtRWArgs {
     public NbtCompound nbt;
-    private final RegistryWrapper.WrapperLookup wrapperLookup;
-
-    @Deprecated
-    public NbtRWArgs(NbtCompound nbt, RegistryWrapper.WrapperLookup wrapperLookup) {
-        this.nbt = nbt;
-        this.wrapperLookup = wrapperLookup;
-    }
 
     public NbtRWArgs(NbtCompound nbt) {
-        this(nbt, null);
+        this.nbt = nbt;
     }
 
     public NbtCompound getNbt() {
         return nbt;
-    }
-
-    @Deprecated
-    public RegistryWrapper.WrapperLookup getWrapperLookup() {
-        return wrapperLookup;
     }
 }
