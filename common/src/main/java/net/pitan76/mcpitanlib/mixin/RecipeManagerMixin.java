fa5f121d8ebbd200dff382f5be26358415d8df58
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-08-30 10:26:17 +0900

    fix

--- common/src/main/java/net/pitan76/mcpitanlib/mixin/RecipeManagerMixin.java
+++ common/src/main/java/net/pitan76/mcpitanlib/mixin/RecipeManagerMixin.java
@@ -2,7 +2,6 @@ package net.pitan76.mcpitanlib.mixin;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
-import com.google.common.collect.Multimap;
 import com.google.gson.JsonElement;
 import net.minecraft.recipe.RecipeEntry;
 import net.minecraft.recipe.RecipeManager;
@@ -13,7 +12,6 @@ import net.minecraft.util.profiler.Profiler;
 import net.pitan76.mcpitanlib.api.event.v0.event.RecipeManagerEvent;
 import net.pitan76.mcpitanlib.api.event.v1.RecipeManagerRegistry;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
