316cdabcb5709561711b5febbbe3df64ba76c098
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-08-31 18:33:31 +0900

    fix network

--- common/src/main/java/net/pitan76/mcpitanlib/api/network/ClientNetworking.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/network/ClientNetworking.java
@@ -2,43 +2,57 @@ package net.pitan76.mcpitanlib.api.network;
 
 import dev.architectury.impl.NetworkAggregator;
 import dev.architectury.networking.NetworkManager;
-import dev.architectury.networking.simple.BaseS2CMessage;
-import dev.architectury.networking.simple.MessageDecoder;
-import dev.architectury.networking.simple.SimpleNetworkManager;
+import dev.architectury.networking.transformers.PacketTransformer;
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
 
-import static dev.architectury.impl.NetworkAggregator.C2S_TYPE;
+import java.util.List;
+import java.util.Objects;
+
+import static dev.architectury.impl.NetworkAggregator.*;
 
 public class ClientNetworking {
     public static void send(Identifier identifier, PacketByteBuf buf) {
-        if (!C2S_TYPE.containsKey(identifier))
-            C2S_TYPE.put(identifier, new CustomPayload.Id<>(identifier));
+        if (!C2S_TYPE.containsKey(identifier)) {
+            CustomPayload.Id type = new CustomPayload.Id<>(identifier);
+            C2S_TYPE.put(identifier, type);
+            registerC2SType(type, NetworkAggregator.BufCustomPacketPayload.streamCodec(type), List.of());
+        }
         CustomPayload payload = new NetworkAggregator.BufCustomPacketPayload(C2S_TYPE.get(identifier), ByteBufUtil.getBytes(buf));
         NetworkManager.sendToServer(payload);
     }
 
     public static void registerReceiver(Identifier identifier, ClientNetworkHandler handler) {
-        SimpleNetworkManager.create(identifier.getNamespace()).registerS2C(identifier.getPath(), new MessageDecoder<>() {
-            @Override
-            public BaseS2CMessage decode(RegistryByteBuf buf) {
-                return null;
-            }
+        CustomPayload.Id<NetworkAggregator.BufCustomPacketPayload> type = new CustomPayload.Id<>(identifier);
+        NetworkManager.registerReceiver(NetworkManager.Side.S2C, type, BufCustomPacketPayload.streamCodec(type), List.of(),
+                (value, context) -> {
+                    RegistryByteBuf buf = new RegistryByteBuf(Unpooled.wrappedBuffer(value.payload()), context.registryAccess());
+                    ClientPlayerEntity player = null;
+                    if (context.getPlayer() instanceof ClientPlayerEntity)
+                        player = (ClientPlayerEntity) context.getPlayer();
 
-            @Override
-            public NetworkManager.NetworkReceiver<RegistryByteBuf> createReceiver() {
-                return ((buf, context) -> handler.receive(MinecraftClient.getInstance(), MinecraftClient.getInstance().player, buf));
-            }
-        });
+                    handler.receive(MinecraftClient.getInstance(), player, buf);
+                    buf.release();
+                });
     }
 
     @FunctionalInterface
     public interface ClientNetworkHandler {
         void receive(MinecraftClient client, ClientPlayerEntity player, PacketByteBuf buf);
+    }
+
+    public static void registerC2SType(CustomPayload.Id<BufCustomPacketPayload> type, PacketCodec<? super RegistryByteBuf, BufCustomPacketPayload> codec, List<PacketTransformer> packetTransformers) {
+        Objects.requireNonNull(type, "Cannot register a null type!");
+        packetTransformers = Objects.requireNonNullElse(packetTransformers, List.of());
+        C2S_CODECS.put(type.id(), (PacketCodec<ByteBuf, ?>) codec);
+        C2S_TRANSFORMERS.put(type.id(), PacketTransformer.concat(packetTransformers));
     }
 }
--- common/src/main/java/net/pitan76/mcpitanlib/api/network/ServerNetworking.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/network/ServerNetworking.java
@@ -2,10 +2,8 @@ package net.pitan76.mcpitanlib.api.network;
 
 import dev.architectury.impl.NetworkAggregator;
 import dev.architectury.networking.NetworkManager;
-import dev.architectury.networking.simple.BaseC2SMessage;
-import dev.architectury.networking.simple.MessageDecoder;
-import dev.architectury.networking.simple.SimpleNetworkManager;
 import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.packet.CustomPayload;
@@ -13,20 +11,35 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 
+import java.util.List;
+
 import static dev.architectury.impl.NetworkAggregator.S2C_TYPE;
+
+import static dev.architectury.impl.NetworkAggregator.BufCustomPacketPayload;
 
 public class ServerNetworking {
     public static void send(ServerPlayerEntity player, Identifier identifier, PacketByteBuf buf) {
-        if (!S2C_TYPE.containsKey(identifier))
-            S2C_TYPE.put(identifier, new CustomPayload.Id<>(identifier));
-        CustomPayload payload = new NetworkAggregator.BufCustomPacketPayload(S2C_TYPE.get(identifier), ByteBufUtil.getBytes(buf));
+        if (!S2C_TYPE.containsKey(identifier)) {
+            CustomPayload.Id type = new CustomPayload.Id<>(identifier);
+            S2C_TYPE.put(identifier, type);
+
+            if (!NetworkAggregator.S2C_CODECS.containsKey(type))
+                NetworkAggregator.registerS2CType(type, BufCustomPacketPayload.streamCodec(type), List.of());
+        }
+
+        CustomPayload payload = new BufCustomPacketPayload(S2C_TYPE.get(identifier), ByteBufUtil.getBytes(buf));
         NetworkManager.sendToPlayer(player, payload);
     }
 
     public static void send(Iterable<ServerPlayerEntity> players, Identifier identifier, PacketByteBuf buf) {
-        if (!S2C_TYPE.containsKey(identifier))
-            S2C_TYPE.put(identifier, new CustomPayload.Id<>(identifier));
-        CustomPayload payload = new NetworkAggregator.BufCustomPacketPayload(S2C_TYPE.get(identifier), ByteBufUtil.getBytes(buf));
+        if (!S2C_TYPE.containsKey(identifier)) {
+            CustomPayload.Id<BufCustomPacketPayload> type = new CustomPayload.Id<>(identifier);
+            S2C_TYPE.put(identifier, type);
+
+            if (!NetworkAggregator.S2C_CODECS.containsKey(type))
+                NetworkAggregator.registerS2CType(type, BufCustomPacketPayload.streamCodec(type), List.of());
+        }
+        CustomPayload payload = new BufCustomPacketPayload(S2C_TYPE.get(identifier), ByteBufUtil.getBytes(buf));
         NetworkManager.sendToPlayers(players, payload);
     }
 
@@ -35,16 +48,16 @@ public class ServerNetworking {
     }
 
     public static void registerReceiver(Identifier identifier, ServerNetworkHandler handler) {
-        SimpleNetworkManager.create(identifier.getNamespace()).registerC2S(identifier.getPath(), new MessageDecoder<>() {
-            @Override
-            public BaseC2SMessage decode(RegistryByteBuf buf) {
-                return null;
-            }
+        CustomPayload.Id<NetworkAggregator.BufCustomPacketPayload> type = new CustomPayload.Id<>(identifier);
+        NetworkManager.registerReceiver(NetworkManager.Side.C2S, type, BufCustomPacketPayload.streamCodec(type), List.of(),
+                (value, context) -> {
+                    RegistryByteBuf buf = new RegistryByteBuf(Unpooled.wrappedBuffer(value.payload()), context.registryAccess());
+                    ServerPlayerEntity player = null;
+                    if (context.getPlayer() instanceof ServerPlayerEntity)
+                        player = (ServerPlayerEntity) context.getPlayer();
 
-            @Override
-            public NetworkManager.NetworkReceiver<RegistryByteBuf> createReceiver() {
-                return ((buf, context) -> handler.receive(context.getPlayer().getServer(), (ServerPlayerEntity) context.getPlayer(), buf));
-            }
+                    handler.receive(context.getPlayer().getServer(), player, buf);
+                    buf.release();
         });
     }
 
