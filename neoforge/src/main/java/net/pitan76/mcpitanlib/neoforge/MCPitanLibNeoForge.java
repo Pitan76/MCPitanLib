6b2d1e24b64a7c24dffaec3b4b6f6be9f82ee934
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-09-01 18:12:10 +0900

    Fix NeoForge

--- neoforge/src/main/java/net/pitan76/mcpitanlib/neoforge/MCPitanLibNeoForge.java
+++ neoforge/src/main/java/net/pitan76/mcpitanlib/neoforge/MCPitanLibNeoForge.java
@@ -1,15 +1,12 @@
 package net.pitan76.mcpitanlib.neoforge;
 
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.pitan76.mcpitanlib.MCPitanLib;
 
 @Mod(MCPitanLib.MOD_ID)
 public class MCPitanLibNeoForge {
-    public MCPitanLibNeoForge() {
-        //EventBuses.registerModEventBus(MCPitanLib.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+    public MCPitanLibNeoForge(ModContainer modContainer) {
         MCPitanLib.init();
-
-        //if (Platform.getEnv().isClient())
-        //    FMLJavaModLoadingContext.get().getModEventBus().addListener(MCPitanLibNeoForgeClient::clientInit);
     }
 }
