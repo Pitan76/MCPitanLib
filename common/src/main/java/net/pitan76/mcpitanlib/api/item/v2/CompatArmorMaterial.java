b9b66f0c035d8757f9cfad872d2af7f79986fcb2
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-09-12 13:29:33 +0900

    add API

---
+++ common/src/main/java/net/pitan76/mcpitanlib/api/item/v2/CompatArmorMaterial.java
@@ -0,0 +1,24 @@
+package net.pitan76.mcpitanlib.api.item.v2;
+
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.util.Identifier;
+import net.pitan76.mcpitanlib.api.item.CompatibleArmorMaterial;
+import net.pitan76.mcpitanlib.api.sound.CompatSoundEvent;
+import net.pitan76.mcpitanlib.api.util.CompatIdentifier;
+
+public interface CompatArmorMaterial extends CompatibleArmorMaterial {
+
+    @Override
+    default SoundEvent getEquipSound() {
+        return getEquipCompatSound().getSoundEvent();
+    }
+
+    CompatSoundEvent getEquipCompatSound();
+
+    @Override
+    default Identifier getId() {
+        return getCompatId().toMinecraft();
+    }
+
+    CompatIdentifier getCompatId();
+}
--- common/src/main/java/net/pitan76/mcpitanlib/api/util/client/ClientUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/client/ClientUtil.java
@@ -1,14 +1,58 @@
 package net.pitan76.mcpitanlib.api.util.client;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.texture.TextureManager;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.resource.ResourceManager;
+import net.pitan76.mcpitanlib.api.entity.Player;
 
 public class ClientUtil {
     public static void setScreen(Screen screen) {
-        MinecraftClient.getInstance().openScreen(screen);
+        getClient().openScreen(screen);
     }
 
     public static Screen getScreen() {
-        return MinecraftClient.getInstance().currentScreen;
+        return getClient().currentScreen;
+    }
+
+    public static Player getPlayer() {
+        return new Player(getClientPlayer());
+    }
+
+    public static ClientPlayerEntity getClientPlayer() {
+        return getClient().player;
+    }
+
+    public static MinecraftClient getClient() {
+        return MinecraftClient.getInstance();
+    }
+
+    public static TextRenderer getTextRenderer() {
+        return getClient().textRenderer;
+    }
+
+    public static ItemRenderer getItemRenderer() {
+        return getClient().getItemRenderer();
+    }
+
+    public static ResourceManager getResourceManager() {
+        return getClient().getResourceManager();
+    }
+
+    public static TextureManager getTextureManager() {
+        return getClient().getTextureManager();
+    }
+
+    public static ClientWorld getWorld() {
+        return getClient().world;
+    }
+
+    public static GameRenderer getGameRenderer() {
+        return getClient().gameRenderer;
     }
 }
