70ed16c4fab435b22ccc645ac568a795003cf6f2
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-09-14 00:43:42 +0900

    add APIs-2

--- fabric/src/main/java/net/pitan76/mcpitanlib/api/client/event/fabric/WorldRenderRegistryImpl.java
+++ fabric/src/main/java/net/pitan76/mcpitanlib/api/client/event/fabric/WorldRenderRegistryImpl.java
@@ -8,6 +8,7 @@ import net.minecraft.util.profiler.Profiler;
 import net.pitan76.mcpitanlib.api.client.event.listener.BeforeBlockOutlineEvent;
 import net.pitan76.mcpitanlib.api.client.event.listener.BeforeBlockOutlineListener;
 import net.pitan76.mcpitanlib.api.client.event.listener.WorldRenderContext;
+import net.pitan76.mcpitanlib.api.client.event.listener.WorldRenderContextListener;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
 
@@ -16,74 +17,130 @@ public class WorldRenderRegistryImpl {
         WorldRenderEvents.BEFORE_BLOCK_OUTLINE.register(((worldRenderContext, hitResult) -> listener.beforeBlockOutline(new BeforeBlockOutlineEvent(
         new WorldRenderContext() {
             @Override
-            public WorldRenderer worldRenderer() {
+            public WorldRenderer getWorldRenderer() {
                 return worldRenderContext.worldRenderer();
             }
 
             @Override
-            public MatrixStack matrixStack() {
+            public MatrixStack getMatrixStack() {
                 return worldRenderContext.matrixStack();
             }
 
             @Override
-            public float tickDelta() {
+            public float getTickDelta() {
                 return worldRenderContext.tickCounter().getTickDelta(true);
             }
 
             @Override
-            public long limitTime() {
-                return (long) worldRenderContext.tickCounter().getLastDuration();
-            }
-
-            @Override
-            public boolean blockOutlines() {
-                return worldRenderContext.blockOutlines();
-            }
-
-            @Override
-            public Camera camera() {
+            public Camera getCamera() {
                 return worldRenderContext.camera();
             }
 
             @Override
-            public GameRenderer gameRenderer() {
+            public GameRenderer getGameRenderer() {
                 return worldRenderContext.gameRenderer();
             }
 
             @Override
-            public LightmapTextureManager lightmapTextureManager() {
+            public LightmapTextureManager getLightmapTextureManager() {
                 return worldRenderContext.lightmapTextureManager();
             }
 
             @Override
-            public Matrix4f projectionMatrix() {
+            public Matrix4f getProjectionMatrix() {
                 return worldRenderContext.projectionMatrix();
             }
 
             @Override
-            public ClientWorld world() {
+            public ClientWorld getWorld() {
                 return worldRenderContext.world();
             }
 
             @Override
-            public Profiler profiler() {
+            public Profiler getProfiler() {
                 return worldRenderContext.profiler();
             }
 
             @Override
-            public boolean advancedTranslucency() {
+            public boolean isAdvancedTranslucency() {
                 return worldRenderContext.advancedTranslucency();
             }
 
             @Override
-            public @Nullable VertexConsumerProvider consumers() {
+            public @Nullable VertexConsumerProvider getConsumers() {
                 return worldRenderContext.consumers();
             }
 
             @Override
-            public @Nullable Frustum frustum() {
+            public @Nullable Frustum getFrustum() {
                 return worldRenderContext.frustum();
             }
         }, hitResult))));
+    }
+
+    public static void registerWorldRenderAfterLevel(WorldRenderContextListener listener) {
+        WorldRenderEvents.END.register((context -> {
+            listener.renderer(new WorldRenderContext() {
+                @Override
+                public WorldRenderer getWorldRenderer() {
+                    return context.worldRenderer();
+                }
+
+                @Override
+                public MatrixStack getMatrixStack() {
+                    return context.matrixStack();
+                }
+
+                @Override
+                public float getTickDelta() {
+                    return context.tickCounter().getTickDelta(true);
+                }
+
+                @Override
+                public Camera getCamera() {
+                    return context.camera();
+                }
+
+                @Override
+                public GameRenderer getGameRenderer() {
+                    return context.gameRenderer();
+                }
+
+                @Override
+                public LightmapTextureManager getLightmapTextureManager() {
+                    return context.lightmapTextureManager();
+                }
+
+                @Override
+                public Matrix4f getProjectionMatrix() {
+                    return context.projectionMatrix();
+                }
+
+                @Override
+                public ClientWorld getWorld() {
+                    return context.world();
+                }
+
+                @Override
+                public Profiler getProfiler() {
+                    return context.profiler();
+                }
+
+                @Override
+                public boolean isAdvancedTranslucency() {
+                    return context.advancedTranslucency();
+                }
+
+                @Override
+                public @Nullable VertexConsumerProvider getConsumers() {
+                    return context.consumers();
+                }
+
+                @Override
+                public @Nullable Frustum getFrustum() {
+                    return context.frustum();
+                }
+            });
+        }));
     }
 }
