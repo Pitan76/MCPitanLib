00c8ba85422ce11944a75be18750f5df4686a20a
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-10-06 17:52:41 +0900

    fix

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/math/BlockRotations.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/math/BlockRotations.java
@@ -14,6 +14,6 @@ public class BlockRotations {
     }
 
     public static BlockRotation random(CompatRandom random) {
-        return BlockRotation.random(random.getMcRandom());
+        return BlockRotation.random(random.getJavaRandom());
     }
 }
