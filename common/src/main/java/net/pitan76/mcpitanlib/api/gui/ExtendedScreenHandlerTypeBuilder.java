ae916b8907b8ab9e09f11990d86c04494be7598d
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-10-09 08:57:21 +0900

    add APIs

--- common/src/main/java/net/pitan76/mcpitanlib/api/gui/ExtendedScreenHandlerTypeBuilder.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/gui/ExtendedScreenHandlerTypeBuilder.java
@@ -5,12 +5,17 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
+import net.pitan76.mcpitanlib.api.gui.args.CreateMenuEvent;
 
-public class ExtendedScreenHandlerTypeBuilder<T extends ExtendedScreenHandler> {
+public class ExtendedScreenHandlerTypeBuilder<T extends ScreenHandler> {
 
     private final Factory<T> factory;
 
     public ExtendedScreenHandlerTypeBuilder(Factory<T> factory) {
+        this.factory = factory;
+    }
+
+    public ExtendedScreenHandlerTypeBuilder(Factory2<T> factory) {
         this.factory = factory;
     }
 
@@ -21,5 +26,15 @@ public class ExtendedScreenHandlerTypeBuilder<T extends ExtendedScreenHandler> {
     @FunctionalInterface
     public interface Factory<T extends ScreenHandler> {
         T create(int syncId, PlayerInventory inventory, PacketByteBuf buf);
+    }
+
+    @FunctionalInterface
+    public interface Factory2<T extends ScreenHandler> extends Factory<T> {
+        T create(CreateMenuEvent e, PacketByteBuf buf);
+
+        @Override
+        default T create(int syncId, PlayerInventory inventory, PacketByteBuf buf) {
+            return create(new CreateMenuEvent(syncId, inventory), buf);
+        }
     }
 }
