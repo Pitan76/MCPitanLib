1e71bb04e337635475882da7ccc7e73d9a2086d9
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-11-09 17:44:34 +0900

    fix1192

--- common/src/main/java/net/pitan76/mcpitanlib/api/tag/v2/CompatTagKey.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/tag/v2/CompatTagKey.java
@@ -1,22 +1,22 @@
 package net.pitan76.mcpitanlib.api.tag.v2;
 
-import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.registry.RegistryEntry;
 import net.pitan76.mcpitanlib.api.tag.TagKey;
 import net.pitan76.mcpitanlib.api.util.CompatIdentifier;
 
 public class CompatTagKey<T> extends TagKey<T> {
     @Deprecated
-    public CompatTagKey(net.minecraft.registry.tag.TagKey<T> tagKey) {
+    public CompatTagKey(net.minecraft.tag.TagKey<T> tagKey) {
         super(tagKey);
     }
 
     @Deprecated
-    public static <T> CompatTagKey<T> of(net.minecraft.registry.tag.TagKey<T> tagKey) {
+    public static <T> CompatTagKey<T> of(net.minecraft.tag.TagKey<T> tagKey) {
         return new CompatTagKey<>(tagKey);
     }
 
     public static <T> CompatTagKey<T> of(CompatTagKeyType<T> type, CompatIdentifier identifier) {
-        return of(net.minecraft.registry.tag.TagKey.of(type.getRegistryKey(), identifier.toMinecraft()));
+        return of(net.minecraft.tag.TagKey.of(type.getRegistryKey(), identifier.toMinecraft()));
     }
 
     public boolean isOf(T value) {
