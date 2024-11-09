a4df4062b650033fb11e0f0bd86098795d326e96
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-11-09 17:17:23 +0900

    add TagAPI and Fix CompatColor

---
+++ common/src/main/java/net/pitan76/mcpitanlib/api/tag/v2/typed/ItemTagKey.java
@@ -0,0 +1,31 @@
+package net.pitan76.mcpitanlib.api.tag.v2.typed;
+
+import net.minecraft.item.Item;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.TagKey;
+import net.pitan76.mcpitanlib.api.tag.v2.CompatTagKey;
+import net.pitan76.mcpitanlib.api.tag.v2.CompatTagKeyType;
+import net.pitan76.mcpitanlib.api.util.CompatIdentifier;
+import net.pitan76.mcpitanlib.api.util.IngredientUtil;
+import net.pitan76.mcpitanlib.api.util.item.ItemUtil;
+
+import java.util.List;
+
+public class ItemTagKey extends CompatTagKey<Item> {
+    @Deprecated
+    public ItemTagKey(TagKey<Item> tagKey) {
+        super(tagKey);
+    }
+
+    public static ItemTagKey of(CompatIdentifier identifier) {
+        return new ItemTagKey(net.minecraft.registry.tag.TagKey.of(CompatTagKeyType.ITEM.getRegistryKey(), identifier.toMinecraft()));
+    }
+
+    public Ingredient asIngredient() {
+        return IngredientUtil.fromTagByIdentifier(getTagKey().id());
+    }
+
+    public List<Item> values() {
+        return ItemUtil.getInTag(this);
+    }
+}
