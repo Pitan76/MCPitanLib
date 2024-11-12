0fbf1a93a329eae09bc044b4866ba2925d5cc9a5
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-11-12 16:03:10 +0900

    fix APIs v3.0.3

--- common/src/main/java/net/pitan76/mcpitanlib/api/item/v3/VanillaCompatToolMaterial.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/item/v3/VanillaCompatToolMaterial.java
@@ -2,18 +2,19 @@ package net.pitan76.mcpitanlib.api.item.v3;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.tag.TagKey;
 import net.pitan76.mcpitanlib.api.tag.item.RepairIngredientTag;
 
 public class VanillaCompatToolMaterial implements CompatToolMaterial {
     private final ToolMaterial material;
 
-    public static final VanillaCompatToolMaterial WOOD = of(ToolMaterial.WOOD);
-    public static final VanillaCompatToolMaterial STONE = of(ToolMaterial.STONE);
-    public static final VanillaCompatToolMaterial IRON = of(ToolMaterial.IRON);
-    public static final VanillaCompatToolMaterial GOLD = of(ToolMaterial.GOLD);
-    public static final VanillaCompatToolMaterial DIAMOND = of(ToolMaterial.DIAMOND);
-    public static final VanillaCompatToolMaterial NETHERITE = of(ToolMaterial.NETHERITE);
+    public static final VanillaCompatToolMaterial WOOD = of(ToolMaterials.WOOD);
+    public static final VanillaCompatToolMaterial STONE = of(ToolMaterials.STONE);
+    public static final VanillaCompatToolMaterial IRON = of(ToolMaterials.IRON);
+    public static final VanillaCompatToolMaterial GOLD = of(ToolMaterials.GOLD);
+    public static final VanillaCompatToolMaterial DIAMOND = of(ToolMaterials.DIAMOND);
+    public static final VanillaCompatToolMaterial NETHERITE = of(ToolMaterials.NETHERITE);
 
     protected VanillaCompatToolMaterial(ToolMaterial material) {
         this.material = material;
@@ -25,39 +26,44 @@ public class VanillaCompatToolMaterial implements CompatToolMaterial {
 
     @Override
     public RepairIngredientTag getRepairIngredientTag() {
-        return new RepairIngredientTag(material.repairItems());
+        if (material == ToolMaterials.STONE) return RepairIngredientTag.STONE_TOOL_MATERIALS;
+        if (material == ToolMaterials.IRON) return RepairIngredientTag.IRON_TOOL_MATERIALS;
+        if (material == ToolMaterials.GOLD) return RepairIngredientTag.GOLDEN_TOOL_MATERIALS;
+        if (material == ToolMaterials.DIAMOND) return RepairIngredientTag.DIAMOND_TOOL_MATERIALS;
+        if (material == ToolMaterials.NETHERITE) return RepairIngredientTag.NETHERITE_TOOL_MATERIALS;
+        return RepairIngredientTag.WOODEN_TOOL_MATERIALS;
     }
 
     @Override
     public int getCompatDurability() {
-        return material.durability();
+        return material.getDurability();
     }
 
     @Override
     public float getCompatMiningSpeedMultiplier() {
-        return material.speed();
+        return material.getMiningSpeedMultiplier();
     }
 
     @Override
     public float getCompatAttackDamage() {
-        return material.attackDamageBonus();
+        return material.getAttackDamage();
     }
 
     @Override
     public int getCompatMiningLevel() {
-        TagKey<Block> tag = material.incorrectBlocksForDrops();
-        if (tag == ToolMaterial.WOOD.incorrectBlocksForDrops()) return 0;
-        if (tag == ToolMaterial.STONE.incorrectBlocksForDrops()) return 1;
-        if (tag == ToolMaterial.IRON.incorrectBlocksForDrops()) return 2;
-        if (tag == ToolMaterial.GOLD.incorrectBlocksForDrops()) return 2;
-        if (tag == ToolMaterial.DIAMOND.incorrectBlocksForDrops()) return 3;
-        if (tag == ToolMaterial.NETHERITE.incorrectBlocksForDrops()) return 4;
+        TagKey<Block> tag = material.getInverseTag();
+        if (tag == ToolMaterials.WOOD.getInverseTag()) return 0;
+        if (tag == ToolMaterials.STONE.getInverseTag()) return 1;
+        if (tag == ToolMaterials.IRON.getInverseTag()) return 2;
+        if (tag == ToolMaterials.GOLD.getInverseTag()) return 2;
+        if (tag == ToolMaterials.DIAMOND.getInverseTag()) return 3;
+        if (tag == ToolMaterials.NETHERITE.getInverseTag()) return 4;
         return -1;
     }
 
     @Override
     public int getCompatEnchantability() {
-        return material.enchantmentValue();
+        return material.getEnchantability();
     }
 
     @Deprecated
