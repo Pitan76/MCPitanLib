b4f94852206947f897f284d40f8bddde6adaec60
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-11-20 14:46:07 +0900

    add APIs

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/client/RenderUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/client/RenderUtil.java
@@ -1,18 +1,12 @@
 package net.pitan76.mcpitanlib.api.util.client;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.gl.ShaderProgram;
-import net.minecraft.client.gl.ShaderProgramKey;
 import net.minecraft.client.gl.ShaderProgramKeys;
-import net.minecraft.client.gui.screen.ingame.AbstractFurnaceScreen;
-import net.minecraft.client.render.GameRenderer;
 import net.minecraft.util.Identifier;
 
 public class RenderUtil {
     public static void setShaderToPositionTexProgram() {
-        try (ShaderProgram program = RenderSystem.setShader(ShaderProgramKeys.POSITION_TEX)) {
-            RenderSystem.setShader(program);
-        }
+        RenderSystem.setShader(ShaderProgramKeys.POSITION_TEX);
     }
 
     public static void setShaderColor(float red, float green, float blue, float alpha) {
