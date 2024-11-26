e76526e1ed9ecf0ed036a229110b96489c510f03
PTOM76 <58260965+ptom76@users.noreply.github.com>
2024-11-26 18:32:00 +0900

    add APIs

--- common/src/main/java/net/pitan76/mcpitanlib/mixin/ItemMixin.java
+++ common/src/main/java/net/pitan76/mcpitanlib/mixin/ItemMixin.java
@@ -18,7 +18,10 @@ import net.pitan76.mcpitanlib.api.event.item.*;
 import net.pitan76.mcpitanlib.api.item.ExtendItemProvider;
 import net.pitan76.mcpitanlib.api.item.ExtendItemProvider.Options;
 import net.pitan76.mcpitanlib.api.item.args.UseActionArgs;
+import net.pitan76.mcpitanlib.api.item.consume.CompatUseAction;
 import net.pitan76.mcpitanlib.api.item.v2.CompatItemProvider;
+import net.pitan76.mcpitanlib.api.util.CompatActionResult;
+import net.pitan76.mcpitanlib.api.util.StackActionResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -34,9 +37,9 @@ public class ItemMixin {
         if (this instanceof ExtendItemProvider) {
             ExtendItemProvider provider = (ExtendItemProvider) this;
             Options options = new Options();
-            TypedActionResult<ItemStack> returnValue = provider.onRightClick(new ItemUseEvent(world, user, hand), options).toTypedActionResult();
+            StackActionResult returnValue = provider.onRightClick(new ItemUseEvent(world, user, hand), options);
             if (options.cancel && returnValue != null)
-                cir.setReturnValue(returnValue);
+                cir.setReturnValue(returnValue.toTypedActionResult());
         }
     }
 
@@ -46,9 +49,9 @@ public class ItemMixin {
             ExtendItemProvider provider = (ExtendItemProvider) this;
             ItemUsageContextMixin contextAccessor = (ItemUsageContextMixin) context;
             Options options = new Options();
-            ActionResult returnValue = provider.onRightClickOnBlock(new ItemUseOnBlockEvent(context.getPlayer(), context.getHand(), contextAccessor.getHit()), options).toActionResult();
+            CompatActionResult returnValue = provider.onRightClickOnBlock(new ItemUseOnBlockEvent(context.getPlayer(), context.getHand(), contextAccessor.getHit()), options);
             if (options.cancel && returnValue != null)
-                cir.setReturnValue(returnValue);
+                cir.setReturnValue(returnValue.toActionResult());
         }
     }
 
@@ -57,9 +60,9 @@ public class ItemMixin {
         if (this instanceof ExtendItemProvider) {
             ExtendItemProvider provider = (ExtendItemProvider) this;
             Options options = new Options();
-            ActionResult returnValue = provider.onRightClickOnEntity(new ItemUseOnEntityEvent(stack, user, entity, hand), options).toActionResult();
+            CompatActionResult returnValue = provider.onRightClickOnEntity(new ItemUseOnEntityEvent(stack, user, entity, hand), options);
             if (options.cancel && returnValue != null)
-                cir.setReturnValue(returnValue);
+                cir.setReturnValue(returnValue.toActionResult());
         }
     }
 
@@ -189,9 +192,9 @@ public class ItemMixin {
         if (this instanceof CompatItemProvider) {
             CompatItemProvider provider = (CompatItemProvider) this;
             Options options = new Options();
-            UseAction returnValue = provider.getUseAction(new UseActionArgs(stack), options).getUseAction();
+            CompatUseAction returnValue = provider.getUseAction(new UseActionArgs(stack), options);
             if (options.cancel)
-                cir.setReturnValue(returnValue);
+                cir.setReturnValue(returnValue.getUseAction());
         }
     }
 }
