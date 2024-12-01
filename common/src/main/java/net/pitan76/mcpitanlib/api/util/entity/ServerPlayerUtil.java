e16c47ce3a7cc9ad0eea8742d490671347f9323d
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-12-01 17:18:11 +0900

    1211-2

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/entity/ServerPlayerUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/entity/ServerPlayerUtil.java
@@ -1,12 +1,12 @@
 package net.pitan76.mcpitanlib.api.util.entity;
 
-import net.minecraft.network.packet.s2c.play.PositionFlag;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.pitan76.mcpitanlib.api.entity.Player;
 import net.pitan76.mcpitanlib.api.sound.CompatSoundCategory;
 import net.pitan76.mcpitanlib.api.sound.CompatSoundEvent;
 
+import java.util.Collections;
 import java.util.Optional;
 
 public class ServerPlayerUtil {
@@ -16,11 +16,12 @@ public class ServerPlayerUtil {
             serverPlayerEntity.setCameraEntity(serverPlayerEntity);
         }
 
-        return serverPlayerEntity.teleport(serverWorld, x, y, z, PositionFlag.VALUES, yaw, pitch);
+        return serverPlayerEntity.teleport(serverWorld, x, y, z, Collections.emptySet(), yaw, pitch);
     }
 
     public static boolean teleport(ServerPlayerEntity serverPlayerEntity, ServerWorld serverWorld, double x, double y, double z, float yaw, float pitch) {
-        return teleport(serverPlayerEntity, serverWorld, x, y, z, yaw, pitch, false);
+        serverPlayerEntity.teleport(serverWorld, x, y, z, yaw, pitch);
+        return true;
     }
 
     public static boolean teleport(ServerPlayerEntity serverPlayerEntity, ServerWorld serverWorld, double x, double y, double z) {
