3e5c1721aeb949f6fab884f6a1e2faadf1ec9e97
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-12-01 17:00:43 +0900

    add APIs

---
+++ fabric/src/main/java/net/pitan76/mcpitanlib/api/transfer/fluid/v1/fabric/FabricFluidVariant.java
@@ -0,0 +1,33 @@
+package net.pitan76.mcpitanlib.api.transfer.fluid.v1.fabric;
+
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.minecraft.fluid.Fluid;
+import net.pitan76.mcpitanlib.api.transfer.fluid.v1.IFluidVariant;
+
+public class FabricFluidVariant implements IFluidVariant {
+
+    public final FluidVariant raw;
+
+    public FabricFluidVariant() {
+        this.raw = FluidVariant.blank();
+    }
+
+    public FabricFluidVariant(FluidVariant variant) {
+        this.raw = variant;
+    }
+
+    @Override
+    public Fluid getFluid() {
+        return raw.getFluid();
+    }
+
+    @Override
+    public boolean isBlank() {
+        return raw.isBlank();
+    }
+
+    @Override
+    public boolean isSame(Fluid fluid) {
+        return raw.isOf(fluid);
+    }
+}
---
+++ neoforge/src/main/java/net/pitan76/mcpitanlib/api/transfer/fluid/v1/neoforge/NeoForgeFluidStorage.java
@@ -0,0 +1,72 @@
+package net.pitan76.mcpitanlib.api.transfer.fluid.v1.neoforge;
+
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
+import net.neoforged.neoforge.fluids.capability.templates.FluidTank;
+import net.pitan76.mcpitanlib.api.event.nbt.WriteNbtArgs;
+import net.pitan76.mcpitanlib.api.transfer.fluid.v1.IFluidStorage;
+import net.pitan76.mcpitanlib.api.transfer.fluid.v1.IFluidVariant;
+
+public class NeoForgeFluidStorage implements IFluidStorage {
+
+    public final FluidTank storage;
+
+    public Runnable onChange;
+
+    public NeoForgeFluidStorage(FluidTank storage, Runnable onChange) {
+        this.storage = storage;
+        this.onChange = onChange;
+    }
+
+    @Override
+    public long getAmount() {
+        return storage.getFluidAmount();
+    }
+
+    @Override
+    public long getCapacity() {
+        return storage.getCapacity();
+    }
+
+    @Override
+    public IFluidVariant getResource() {
+        return new NeoForgeFluidVariant(storage.getFluid());
+    }
+
+    @Override
+    public void setResource(IFluidVariant variant) {
+        storage.setFluid(((NeoForgeFluidVariant) variant).raw);
+    }
+
+    @Override
+    public boolean isResourceBlank() {
+        return storage.isEmpty();
+    }
+
+    @Override
+    public long insert(IFluidVariant variant, long maxAmount, boolean simulate) {
+        if (simulate)
+            return storage.fill(((NeoForgeFluidVariant) variant).raw.copyWithAmount((int) maxAmount), IFluidHandler.FluidAction.SIMULATE);
+
+        onChange.run();
+        return storage.fill(((NeoForgeFluidVariant) variant).raw.copyWithAmount((int) maxAmount), IFluidHandler.FluidAction.EXECUTE);
+    }
+
+    @Override
+    public long extract(IFluidVariant variant, long maxAmount, boolean simulate) {
+        if (simulate)
+            return storage.drain(((NeoForgeFluidVariant) variant).raw.copyWithAmount((int) maxAmount), IFluidHandler.FluidAction.SIMULATE).getAmount();
+
+        onChange.run();
+        return storage.drain(((NeoForgeFluidVariant) variant).raw.copyWithAmount((int) maxAmount), IFluidHandler.FluidAction.EXECUTE).getAmount();
+    }
+
+    @Override
+    public void writeNbt(WriteNbtArgs args) {
+        storage.writeToNBT(args.getWrapperLookup(), args.nbt);
+    }
+
+    @Override
+    public void readNbt(WriteNbtArgs args) {
+        storage.readFromNBT(args.getWrapperLookup(), args.nbt);
+    }
+}
