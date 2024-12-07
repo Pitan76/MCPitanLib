a06ab8f81268d3075e455cd59180a2d70557c2b7
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-12-08 00:03:14 +0900

    1211

--- common/src/main/java/net/pitan76/mcpitanlib/midohra/recipe/CraftingRecipe.java
+++ common/src/main/java/net/pitan76/mcpitanlib/midohra/recipe/CraftingRecipe.java
@@ -2,6 +2,7 @@ package net.pitan76.mcpitanlib.midohra.recipe;
 
 import net.minecraft.recipe.book.CraftingRecipeCategory;
 import net.minecraft.util.collection.DefaultedList;
+import net.pitan76.mcpitanlib.api.registry.CompatRegistryLookup;
 import net.pitan76.mcpitanlib.api.util.collection.ItemStackList;
 import net.pitan76.mcpitanlib.midohra.item.ItemStack;
 import net.pitan76.mcpitanlib.midohra.recipe.input.CraftingRecipeInputOrInventory;
