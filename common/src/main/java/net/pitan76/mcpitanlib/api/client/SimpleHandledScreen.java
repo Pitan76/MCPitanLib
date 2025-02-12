1054cdc20bfbea72f88b4572962eabdb331e0a07
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2025-02-12 14:30:19 +0900

    fix APIs

--- common/src/main/java/net/pitan76/mcpitanlib/api/client/SimpleHandledScreen.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/SimpleHandledScreen.java
@@ -19,6 +19,7 @@ import net.pitan76.mcpitanlib.api.client.render.DrawObjectDM;
 import net.pitan76.mcpitanlib.api.client.render.handledscreen.*;
 import net.pitan76.mcpitanlib.api.client.render.screen.RenderBackgroundTextureArgs;
 import net.pitan76.mcpitanlib.api.util.CompatIdentifier;
+import net.pitan76.mcpitanlib.api.util.IdentifierUtil;
 import net.pitan76.mcpitanlib.api.util.client.ClientUtil;
 import net.pitan76.mcpitanlib.api.util.client.RenderUtil;
 
@@ -243,7 +244,7 @@ public abstract class SimpleHandledScreen extends HandledScreen<ScreenHandler> {
     }
 
     public Identifier getBackgroundTexture() {
-        return getCompatBackgroundTexture().toMinecraft();
+        return IdentifierUtil.from(getCompatBackgroundTexture());
     }
 
     public CompatIdentifier getCompatBackgroundTexture() {
