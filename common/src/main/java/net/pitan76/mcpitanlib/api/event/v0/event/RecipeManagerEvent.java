30ddf323120a7e090fa3df2b87704a626aecf3c7
PTOM76 <ptms76@outlook.jp>
2025-04-06 18:23:29 +0900

    add APIs

--- common/src/main/java/net/pitan76/mcpitanlib/api/event/v0/event/RecipeManagerEvent.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/event/v0/event/RecipeManagerEvent.java
@@ -9,6 +9,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.profiler.Profiler;
 import net.pitan76.mcpitanlib.api.recipe.CompatibleRecipeEntry;
 import net.pitan76.mcpitanlib.api.recipe.v2.CompatRecipeEntry;
+import net.pitan76.mcpitanlib.api.recipe.v3.CompatRecipe;
 
 import java.util.Map;
 
@@ -44,6 +45,10 @@ public class RecipeManagerEvent {
         return resourceManager;
     }
 
+    public net.pitan76.mcpitanlib.midohra.resource.ResourceManager getResourceManagerM() {
+        return net.pitan76.mcpitanlib.midohra.resource.ResourceManager.of(resourceManager);
+    }
+
     public void putCompatibleRecipeEntry(Identifier id, CompatibleRecipeEntry entry) {
         map.get(entry.getType()).put(id, entry.getRecipe());
     }
@@ -54,5 +59,13 @@ public class RecipeManagerEvent {
 
     public void putCompatibleRecipeEntry(CompatRecipeEntry<?> entry) {
         map.get(entry.getType()).put(entry.getId(), entry.getRecipe());
+    }
+
+    public void putRecipeEntry(net.pitan76.mcpitanlib.midohra.recipe.entry.RecipeEntry entry) {
+        map.get(entry.getRawRecipeType()).put(entry.getId().toMinecraft(), entry.toMinecraft());
+    }
+
+    public void putRecipe(CompatRecipe recipe) {
+        map.get(recipe.getType()).put(recipe.getId(), recipe.getRecipeEntry().getRecipeEntry());
     }
 }
