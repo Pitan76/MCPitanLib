c76ad972d0f5e397218c0fdd1e7cc368ebc9a6e0
PTOM76 <ptms76@outlook.jp>
2025-04-20 16:31:51 +0900

    [cp] rename isExit to isPresent

--- common/src/main/java/net/pitan76/mcpitanlib/midohra/item/ItemWrapper.java
+++ common/src/main/java/net/pitan76/mcpitanlib/midohra/item/ItemWrapper.java
@@ -33,17 +33,17 @@ public class ItemWrapper {
     }
 
     public static ItemWrapper of(CompatIdentifier id, CompatIdentifier... ids) {
-        if (of(id).isExist()) return of(id);
+        if (of(id).isPresent()) return of(id);
 
         for (CompatIdentifier id1 : ids) {
-            if (of(id1).isExist())
+            if (of(id1).isPresent())
                 return of(id1);
         }
 
         return of();
     }
 
-    public boolean isExist() {
+    public boolean isPresent() {
         return !isEmpty();
     }
 
