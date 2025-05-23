85c862e957b07522d110e71c6029a91f954e0b1f
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2025-05-23 22:02:34 +0900

    [cp] add apis rendering v2

--- common/src/main/java/net/pitan76/mcpitanlib/api/client/render/block/entity/CompatBlockEntityRenderer.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/render/block/entity/CompatBlockEntityRenderer.java
@@ -16,4 +16,23 @@ public interface CompatBlockEntityRenderer<T extends CompatBlockEntity> extends 
         render(new BlockEntityRenderEvent<>(entity, tickProgress, matrices, vertexConsumers, light, overlay));
     }
 
+    default boolean rendersOutsideBoundingBoxOverride(T blockEntity) {
+        return BlockEntityRenderer.super.rendersOutsideBoundingBox(blockEntity);
+    }
+
+    default int getRenderDistanceOverride() {
+        return BlockEntityRenderer.super.getRenderDistance();
+    }
+
+    @Deprecated
+    @Override
+    default boolean rendersOutsideBoundingBox(T blockEntity) {
+        return rendersOutsideBoundingBoxOverride(blockEntity);
+    }
+
+    @Deprecated
+    @Override
+    default int getRenderDistance() {
+        return getRenderDistanceOverride();
+    }
 }
