ef7108bd43f0ee6c328f506993143b41b13280c3
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2025-05-23 22:05:55 +0900

    [cp] add apis rendering

---
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/client/render/VertexConsumerUtil.java
@@ -0,0 +1,83 @@
+package net.pitan76.mcpitanlib.api.util.client.render;
+
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.pitan76.mcpitanlib.api.client.render.DrawObjectMV;
+
+public class VertexConsumerUtil {
+    public static VertexConsumer vertex(VertexConsumer vertexConsumer, float x, float y, float z) {
+        return vertexConsumer.vertex(x, y, z);
+    }
+
+    public static VertexConsumer vertex(VertexConsumer vertexConsumer, MatrixStack stack, float x, float y, float z) {
+        return vertexConsumer.vertex(stack.peek(), x, y, z);
+    }
+
+    public static VertexConsumer normal(VertexConsumer vertexConsumer, float x, float y, float z) {
+        return vertexConsumer.normal(x, y, z);
+    }
+
+    public static VertexConsumer color(VertexConsumer vertexConsumer, float red, float green, float blue, float alpha) {
+        return vertexConsumer.color(red, green, blue, alpha);
+    }
+
+    public static VertexConsumer color(VertexConsumer vertexConsumer, int red, int green, int blue, int alpha) {
+        return vertexConsumer.color(red, green, blue, alpha);
+    }
+
+    public static VertexConsumer colorARGB(VertexConsumer vertexConsumer, int argb) {
+        return vertexConsumer.color(argb);
+    }
+
+    public static VertexConsumer colorRGB(VertexConsumer vertexConsumer, int rgb) {
+        return vertexConsumer.colorRgb(rgb);
+    }
+
+    public static VertexConsumer light(VertexConsumer vertexConsumer, int light) {
+        return vertexConsumer.light(light);
+    }
+
+    public static VertexConsumer overlay(VertexConsumer vertexConsumer, int overlay) {
+        return vertexConsumer.overlay(overlay);
+    }
+
+    public static DrawObjectMV vertex(DrawObjectMV drawObject, float x, float y, float z) {
+        vertex(drawObject.getBuffer(), drawObject.getStack(), x, y, z);
+        return drawObject;
+    }
+
+    public static DrawObjectMV normal(DrawObjectMV drawObject, float x, float y, float z) {
+        normal(drawObject.getBuffer(), x, y, z);
+        return drawObject;
+    }
+
+    public static DrawObjectMV color(DrawObjectMV drawObject, float red, float green, float blue, float alpha) {
+        color(drawObject.getBuffer(), red, green, blue, alpha);
+        return drawObject;
+    }
+
+    public static DrawObjectMV color(DrawObjectMV drawObject, int red, int green, int blue, int alpha) {
+        color(drawObject.getBuffer(), red, green, blue, alpha);
+        return drawObject;
+    }
+
+    public static DrawObjectMV colorARGB(DrawObjectMV drawObject, int argb) {
+        colorARGB(drawObject.getBuffer(), argb);
+        return drawObject;
+    }
+
+    public static DrawObjectMV colorRGB(DrawObjectMV drawObject, int rgb) {
+        colorRGB(drawObject.getBuffer(), rgb);
+        return drawObject;
+    }
+
+    public static DrawObjectMV light(DrawObjectMV drawObject, int light) {
+        light(drawObject.getBuffer(), light);
+        return drawObject;
+    }
+
+    public static DrawObjectMV overlay(DrawObjectMV drawObject, int overlay) {
+        overlay(drawObject.getBuffer(), overlay);
+        return drawObject;
+    }
+}
