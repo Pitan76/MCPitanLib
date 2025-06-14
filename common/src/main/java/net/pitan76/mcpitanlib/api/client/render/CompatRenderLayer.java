a651726819f31ca9e3395c99ac43cf5f78fd670a
PTOM76 <ptms76@outlook.jp>
2025-06-14 17:07:20 +0900

    [cp] feat: enhance rendering utilities and add WorldRendererUtil for lightmap coordinates

--- common/src/main/java/net/pitan76/mcpitanlib/api/client/render/CompatRenderLayer.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/render/CompatRenderLayer.java
@@ -1,5 +1,7 @@
 package net.pitan76.mcpitanlib.api.client.render;
 
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import net.minecraft.client.render.BuiltBuffer;
 import net.minecraft.client.render.RenderLayer;
 import net.pitan76.mcpitanlib.api.util.CompatIdentifier;
 
@@ -37,5 +39,17 @@ public class CompatRenderLayer {
 
     public static CompatRenderLayer getArmorCutoutNoCull(CompatIdentifier id) {
         return new CompatRenderLayer(RenderLayer.getArmorCutoutNoCull(id.toMinecraft()));
+    }
+
+    public static CompatRenderLayer getEntitySolid(CompatIdentifier id) {
+        return new CompatRenderLayer(RenderLayer.getEntitySolid(id.toMinecraft()));
+    }
+
+    public static CompatRenderLayer create(String name, int size, boolean hasCrumbling, boolean translucent, RenderPipeline pipeline, RenderLayer.MultiPhaseParameters params) {
+        return new CompatRenderLayer(RenderLayer.of(name, size, hasCrumbling, translucent, pipeline, params));
+    }
+
+    public void draw(BuiltBuffer buffer) {
+        layer.draw(buffer);
     }
 }
--- common/src/main/java/net/pitan76/mcpitanlib/api/util/client/render/VertexRenderingUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/client/render/VertexRenderingUtil.java
@@ -4,6 +4,8 @@ import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.pitan76.mcpitanlib.api.client.render.DrawObjectMV;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
 
 public class VertexRenderingUtil {
     public static void drawBox(MatrixStack matrices, VertexConsumer buffer, double minX, double minY, double minZ, double maxX, double maxY, double maxZ, float red, float green, float blue, float alpha) {
@@ -29,5 +31,13 @@ public class VertexRenderingUtil {
 
     public static void drawFilledBox(DrawObjectMV drawObject, double minX, double minY, double minZ, double maxX, double maxY, double maxZ, float red, float green, float blue, float alpha) {
         drawFilledBox(drawObject.getStack(), drawObject.getBuffer(), minX, minY, minZ, maxX, maxY, maxZ, red, green, blue, alpha);
+    }
+
+    public static void renderQuad(VertexConsumer vertexConsumer, MatrixStack stack, Matrix4f matrix4f, Matrix3f matrix3f, float x1, float y1, float z1, float x2, float y2, float z2, float normalX, float normalY, float normalZ, int r, int g, int b, int alpha, int u, int v, int overlay, int light) {
+        VertexConsumerUtil.renderQuad(vertexConsumer, stack, matrix4f, matrix3f, x1, y1, z1, x2, y2, z2, normalX, normalY, normalZ, r, g, b, alpha, u, v, overlay, light);
+    }
+
+    public static void renderQuad(DrawObjectMV drawObject, float x1, float y1, float z1, float x2, float y2, float z2, float normalX, float normalY, float normalZ, int r, int g, int b, int alpha, int u, int v, int overlay, int light) {
+        renderQuad(drawObject.getBuffer(), drawObject.getStack(), drawObject.getMatrix4f(), drawObject.getMatrix3f(), x1, y1, z1, x2, y2, z2, normalX, normalY, normalZ, r, g, b, alpha, u, v, overlay, light);
     }
 }
