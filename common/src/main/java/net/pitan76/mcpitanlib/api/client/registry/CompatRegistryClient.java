0d0d2806a65b6cc737d1e37ddd979423224376f2
PTOM76 <ptms76@outlook.jp>
2025-06-22 10:40:00 +0900

    [cp] feat: add methods to register render types for blocks and fluids in CompatRegistryClient

--- common/src/main/java/net/pitan76/mcpitanlib/api/client/registry/CompatRegistryClient.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/registry/CompatRegistryClient.java
@@ -42,6 +42,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.random.Random;
 import net.pitan76.mcpitanlib.MCPitanLib;
+import net.pitan76.mcpitanlib.api.client.render.CompatRenderLayer;
 import net.pitan76.mcpitanlib.api.client.render.EntityModelLayerContext;
 
 import java.util.List;
@@ -198,5 +199,13 @@ public class CompatRegistryClient {
         BlockEntityRendererRegistry.register(type, ctx -> provider.create(new BlockEntityRendererFactory.Context(
                 ctx.getRenderDispatcher(), ctx.getRenderManager(), ctx.getItemRenderer(), ctx.getEntityRenderDispatcher(), ctx.getLayerRenderDispatcher(), ctx.getTextRenderer()
         )));
+    }
+
+    public static void registerRenderTypeBlock(CompatRenderLayer layer, Block block) {
+        registerRenderTypeBlock(layer.layer, block);
+    }
+
+    public static void registerRenderTypeFluid(CompatRenderLayer layer, Fluid fluid) {
+        registerRenderTypeFluid(layer.layer, fluid);
     }
 }
