9739707dba2901aad48bdfd63fe98ead507e459c
PTOM76 <ptms76@outlook.jp>
2025-06-22 11:48:09 +0900

    [cp] add bat

---
+++ pull.bat
@@ -0,0 +1,54 @@
+git fetch origin
+git checkout 1.21.6
+timeout /T 1
+git pull
+
+git fetch origin
+git checkout 1.21.5
+timeout /T 1
+git pull
+
+git fetch origin
+git checkout 1.21.4
+timeout /T 1
+git pull
+
+git fetch origin
+git checkout 1.21.3
+timeout /T 1
+git pull
+
+git fetch origin
+git checkout 1.21.1
+timeout /T 1
+git pull
+
+git fetch origin
+git checkout 1.20.4
+timeout /T 1
+git pull
+
+git fetch origin
+git checkout 1.20.1
+timeout /T 1
+git pull
+
+git fetch origin
+git checkout 1.19.2
+timeout /T 1
+git pull
+
+git fetch origin
+git checkout 1.18.2
+timeout /T 1
+git pull
+
+git fetch origin
+git checkout 1.16.5
+timeout /T 1
+git pull
+
+git fetch origin
+git checkout 1.21.6
+
+pause
