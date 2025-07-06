a5b96d3fa1b0d6d575edfe91a3ff676e2c22ef0e
PTOM76 <ptms76@outlook.jp>
2025-07-06 17:24:47 +0900

    1216

--- common/src/main/java/net/pitan76/mcpitanlib/core/registry/MCPLRegistry1_21.java
+++ common/src/main/java/net/pitan76/mcpitanlib/core/registry/MCPLRegistry1_21.java
@@ -5,6 +5,7 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.component.ComponentType;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.pitan76.mcpitanlib.midohra.world.chunk.ChunkTicketType;
 
 import java.util.function.Supplier;
 
@@ -12,12 +13,14 @@ import java.util.function.Supplier;
 public class MCPLRegistry1_21 {
 
     public final DeferredRegister<ComponentType<?>> DATA_COMPONENT_TYPE;
+    public final DeferredRegister<net.minecraft.server.world.ChunkTicketType> TICKET_TYPE;
 
     private final MCPLRegistry mcplr;
 
     public MCPLRegistry1_21(MCPLRegistry mcplr, String MOD_ID) {
         this.mcplr = mcplr;
         DATA_COMPONENT_TYPE = DeferredRegister.create(MOD_ID, RegistryKeys.DATA_COMPONENT_TYPE);
+        TICKET_TYPE = DeferredRegister.create(MOD_ID, RegistryKeys.TICKET_TYPE);
     }
 
     public void register() {
@@ -26,5 +29,9 @@ public class MCPLRegistry1_21 {
 
     public RegistrySupplier<ComponentType<?>> registryDataComponentType(Identifier id, Supplier<ComponentType<?>> supplier) {
         return DATA_COMPONENT_TYPE.register(id, supplier);
+    }
+
+    public Supplier<ChunkTicketType<?>> registryChunkTicketType(Identifier id, Supplier<ChunkTicketType<?>> supplier) {
+        return () -> ChunkTicketType.of(TICKET_TYPE.register(id, () -> supplier.get().getRaw()).get());
     }
 }
