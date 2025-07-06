cdbc0282e2df9e1fbb0b211fa5c53bb07b14d631
PTOM76 <ptms76@outlook.jp>
2025-07-06 10:34:04 +0900

    [cp] fix readNbt method to use ReadNbtArgs

--- fabric/src/main/java/net/pitan76/mcpitanlib/api/transfer/fluid/v1/fabric/FabricFluidStorage.java
+++ fabric/src/main/java/net/pitan76/mcpitanlib/api/transfer/fluid/v1/fabric/FabricFluidStorage.java
@@ -2,6 +2,7 @@ package net.pitan76.mcpitanlib.api.transfer.fluid.v1.fabric;
 
 import net.fabricmc.fabric.api.transfer.v1.fluid.base.SingleFluidStorage;
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction;
+import net.pitan76.mcpitanlib.api.event.nbt.ReadNbtArgs;
 import net.pitan76.mcpitanlib.api.event.nbt.WriteNbtArgs;
 import net.pitan76.mcpitanlib.api.transfer.fluid.v1.IFluidStorage;
 import net.pitan76.mcpitanlib.api.transfer.fluid.v1.IFluidVariant;
@@ -71,7 +72,7 @@ public class FabricFluidStorage implements IFluidStorage {
     }
 
     @Override
-    public void readNbt(WriteNbtArgs args) {
+    public void readNbt(ReadNbtArgs args) {
         storage.readNbt(args.nbt, args.getWrapperLookup());
 
     }
