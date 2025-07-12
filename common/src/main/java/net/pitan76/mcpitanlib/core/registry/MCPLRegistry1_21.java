6e9612044dfda180864befc0f0cf4937bec5f010
PTOM76 <ptms76@outlook.jp>
2025-07-12 10:46:50 +0900

    [cp] remove ticket register

--- common/src/main/java/net/pitan76/mcpitanlib/core/registry/MCPLRegistry1_21.java
+++ common/src/main/java/net/pitan76/mcpitanlib/core/registry/MCPLRegistry1_21.java
@@ -22,7 +22,6 @@ public class MCPLRegistry1_21 {
 
     public void register() {
         DATA_COMPONENT_TYPE.register();
-        TICKET_TYPE.register();
     }
 
     public RegistrySupplier<ComponentType<?>> registryDataComponentType(Identifier id, Supplier<ComponentType<?>> supplier) {
