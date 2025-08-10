2e023232a424ca62961628b04ce3357b648a4ed2
PTOM76 <ptms76@outlook.jp>
2025-08-10 20:51:40 +0900

    [cp] add APIs (Equippable, EntityAttribute, AttributeModifier)

---
+++ common/src/main/java/net/pitan76/mcpitanlib/api/entity/attribute/CompatEntityAttributeInstance.java
@@ -0,0 +1,70 @@
+package net.pitan76.mcpitanlib.api.entity.attribute;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.pitan76.mcpitanlib.api.entity.Player;
+import net.pitan76.mcpitanlib.api.util.CompatIdentifier;
+
+import java.util.function.Consumer;
+
+public class CompatEntityAttributeInstance {
+    private final net.minecraft.entity.attribute.EntityAttributeInstance raw;
+
+    @Deprecated
+    public CompatEntityAttributeInstance(net.minecraft.entity.attribute.EntityAttributeInstance instance) {
+        this.raw = instance;
+    }
+
+    @Deprecated
+    public net.minecraft.entity.attribute.EntityAttributeInstance raw() {
+        return raw;
+    }
+
+    @Deprecated
+    public static CompatEntityAttributeInstance of(net.minecraft.entity.attribute.EntityAttributeInstance instance) {
+        return new CompatEntityAttributeInstance(instance);
+    }
+
+    public static CompatEntityAttributeInstance create(CompatEntityAttribute type, Consumer<CompatEntityAttributeInstance> updateCallback) {
+        return of(new EntityAttributeInstance(type.raw(), modifier -> {
+            if (updateCallback != null)
+                updateCallback.accept(of(modifier));
+        }));
+    }
+
+    public static CompatEntityAttributeInstance get(LivingEntity entity, CompatEntityAttribute attribute) {
+        return new CompatEntityAttributeInstance(entity.getAttributeInstance(attribute.raw()));
+    }
+
+    public static CompatEntityAttributeInstance get(Player player, CompatEntityAttribute attribute) {
+        return get(player.getEntity(), attribute);
+    }
+
+    public boolean hasModifier(CompatIdentifier id) {
+        return raw().hasModifier(id.toMinecraft());
+    }
+
+    public double getValue() {
+        return raw().getValue();
+    }
+
+    public void setBaseValue(double value) {
+        raw().setBaseValue(value);
+    }
+
+    public void addPersistentModifier(CompatEntityAttributeModifier modifier) {
+        raw().addPersistentModifier(modifier.raw());
+    }
+
+    public void removeModifier(CompatIdentifier id) {
+        raw().removeModifier(id.toMinecraft());
+    }
+
+    public CompatEntityAttribute getAttribute() {
+        return CompatEntityAttribute.of(raw().getAttribute());
+    }
+
+    public boolean isNull() {
+        return raw() == null;
+    }
+}
