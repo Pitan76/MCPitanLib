fc6a0e6283b9ada0d9aae84e8e5197d592c4b598
PTOM76 <ptms76@outlook.jp>
2025-10-19 14:27:14 +0900

    [cp] HitResult API

--- common/src/main/java/net/pitan76/mcpitanlib/api/event/entity/CollisionEvent.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/event/entity/CollisionEvent.java
@@ -2,6 +2,7 @@ package net.pitan76.mcpitanlib.api.event.entity;
 
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.Vec3d;
+import net.pitan76.mcpitanlib.midohra.util.hit.HitResultType;
 
 public class CollisionEvent {
 
@@ -21,5 +22,13 @@ public class CollisionEvent {
 
     public Vec3d getPos() {
         return hitResult.getPos();
+    }
+
+    public net.pitan76.mcpitanlib.midohra.util.hit.HitResult getHitResultM() {
+        return net.pitan76.mcpitanlib.midohra.util.hit.HitResult.of(getHitResult());
+    }
+
+    public HitResultType getHitResultTypeM() {
+        return HitResultType.from(getType());
     }
 }
