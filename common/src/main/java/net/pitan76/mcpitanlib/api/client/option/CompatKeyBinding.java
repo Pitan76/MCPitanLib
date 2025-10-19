4dd1f726d42d6b202189f407a5dd3e90287a2a7b
PTOM76 <ptms76@outlook.jp>
2025-10-19 17:12:27 +0900

    CompatKeyBinding 1218

--- common/src/main/java/net/pitan76/mcpitanlib/api/client/option/CompatKeyBinding.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/option/CompatKeyBinding.java
@@ -4,45 +4,24 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.text.Text;
 import net.pitan76.mcpitanlib.api.util.CompatIdentifier;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 
 public class CompatKeyBinding {
     private final KeyBinding keyBinding;
-    public static final Map<CompatIdentifier, KeyBinding.Category> categories = new HashMap<>();
 
     public CompatKeyBinding(KeyBinding keyBinding) {
         this.keyBinding = keyBinding;
     }
 
     public CompatKeyBinding(String translationKey, int defaultKeyCode, CompatIdentifier category) {
-        KeyBinding.Category cat;
-
-        if (categories.containsKey(category)) {
-            cat = categories.get(category);
-        } else {
-            cat = KeyBinding.Category.create(category.toMinecraft());
-            categories.put(category, cat);
-        }
-
-        this.keyBinding = new KeyBinding(translationKey, defaultKeyCode, cat);
+        this.keyBinding = new KeyBinding(translationKey, defaultKeyCode, "key.category." + category.getNamespace() + "." + category.getPath());
     }
 
     public CompatKeyBinding(String translationKey, int defaultKeyCode) {
         String[] parts = translationKey.split("\\.");
         if (Objects.equals(parts[0], "key") && parts.length == 3) {
             CompatIdentifier category = CompatIdentifier.of(parts[1], "main");
-            KeyBinding.Category cat;
-
-            if (categories.containsKey(category)) {
-                cat = categories.get(category);
-            } else {
-                cat = KeyBinding.Category.create(category.toMinecraft());
-                categories.put(category, cat);
-            }
-
-            this.keyBinding = new KeyBinding(translationKey, defaultKeyCode, cat);
+            this.keyBinding = new KeyBinding(translationKey, defaultKeyCode, "key.category." + category.getNamespace() + "." + category.getPath());
         } else {
             throw new IllegalArgumentException("Cannot infer category from translation key: " + translationKey);
         }
