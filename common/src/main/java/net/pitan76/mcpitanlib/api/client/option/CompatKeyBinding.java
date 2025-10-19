13f85c91861aa3e437ae3b500e6d565225b18c9a
PTOM76 <ptms76@outlook.jp>
2025-10-19 17:17:15 +0900

    [cp] CompatKeyBinding

---
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/option/CompatKeyBinding.java
@@ -0,0 +1,57 @@
+package net.pitan76.mcpitanlib.api.client.option;
+
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.text.Text;
+import net.pitan76.mcpitanlib.api.util.CompatIdentifier;
+
+import java.util.Objects;
+
+public class CompatKeyBinding {
+    private final KeyBinding keyBinding;
+
+    public CompatKeyBinding(KeyBinding keyBinding) {
+        this.keyBinding = keyBinding;
+    }
+
+    public CompatKeyBinding(String translationKey, int defaultKeyCode, CompatIdentifier category) {
+        this.keyBinding = new KeyBinding(translationKey, defaultKeyCode, "key.category." + category.getNamespace() + "." + category.getPath());
+    }
+
+    public CompatKeyBinding(String translationKey, int defaultKeyCode) {
+        String[] parts = translationKey.split("\\.");
+        if (Objects.equals(parts[0], "key") && parts.length == 3) {
+            CompatIdentifier category = CompatIdentifier.of(parts[1], "main");
+            this.keyBinding = new KeyBinding(translationKey, defaultKeyCode, "key.category." + category.getNamespace() + "." + category.getPath());
+        } else {
+            throw new IllegalArgumentException("Cannot infer category from translation key: " + translationKey);
+        }
+    }
+
+    public String getTranslationKey() {
+        return keyBinding.getBoundKeyTranslationKey();
+    }
+
+    public Text getBoundKeyLocalizedText() {
+        return keyBinding.getBoundKeyLocalizedText();
+    }
+
+    public int getDefaultKeyCode() {
+        return keyBinding.getDefaultKey().getCode();
+    }
+
+    public static CompatKeyBinding of(String translationKey, int defaultKeyCode, CompatIdentifier category) {
+        return new CompatKeyBinding(translationKey, defaultKeyCode, category);
+    }
+
+    public static CompatKeyBinding of(String translationKey, int defaultKeyCode) {
+        return new CompatKeyBinding(translationKey, defaultKeyCode);
+    }
+
+    public KeyBinding toMinecraft() {
+        return keyBinding;
+    }
+
+    public KeyBinding getRaw() {
+        return keyBinding;
+    }
+}
