a24f0215cb439c3396188ec634d6710a4d1206cf
PTOM76 <ptms76@outlook.jp>
2025-10-19 16:57:57 +0900

    [cp] fix ContainerUser

--- common/src/main/java/net/pitan76/mcpitanlib/api/entity/CompatContainerUser.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/entity/CompatContainerUser.java
@@ -1,17 +1,28 @@
 package net.pitan76.mcpitanlib.api.entity;
 
-import net.minecraft.entity.ContainerUser;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class CompatContainerUser {
-    protected ContainerUser containerUser;
+    protected PlayerEntity containerUser;
+
+    public CompatContainerUser(PlayerEntity containerUser) {
+        this.containerUser = containerUser;
+    }
 
     public double getContainerInteractionRange() {
-        // Player = 4.5d
-        return containerUser.getContainerInteractionRange();
+        return 4.5d;
     }
 
     public LivingEntity asLivingEntity() {
-        return containerUser.asLivingEntity();
+        return containerUser;
+    }
+
+    public boolean isPlayer() {
+        return true;
+    }
+
+    public Player asPlayer() {
+        return new Player(containerUser);
     }
 }
