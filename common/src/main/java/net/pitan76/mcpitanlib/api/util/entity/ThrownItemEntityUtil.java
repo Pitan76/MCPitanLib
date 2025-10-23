c5b7cf1257e8f4c1060df934674d356447facb13
PTOM76 <58260965+ptom76@users.noreply.github.com>
2025-10-23 22:16:24 +0900

    [cp] Add APIs, Fix APIs

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/entity/ThrownItemEntityUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/entity/ThrownItemEntityUtil.java
@@ -1,7 +1,9 @@
 package net.pitan76.mcpitanlib.api.util.entity;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.ItemStack;
+import net.pitan76.mcpitanlib.api.entity.CompatThrownItemEntity;
 
 public class ThrownItemEntityUtil {
     public static ItemStack getItem(ThrownItemEntity entity) {
@@ -14,5 +16,21 @@ public class ThrownItemEntityUtil {
 
     public static ItemStack getStack(ThrownItemEntity entity) {
         return entity.getStack();
+    }
+
+    public static void setVelocity(ThrownItemEntity entity, Entity shooter, float pitch, float yaw, float roll, float speed, float divergence) {
+        ProjectileEntityUtil.setVelocity(entity, shooter, pitch, yaw, roll, speed, divergence);
+    }
+
+    public static void setVelocity(CompatThrownItemEntity entity, Entity shooter, float pitch, float yaw, float roll, float speed, float divergence) {
+        ProjectileEntityUtil.setVelocity(entity, shooter, pitch, yaw, roll, speed, divergence);
+    }
+
+    public static void setVelocity(ThrownItemEntity entity, double x, double y, double z, float power, float uncertainty) {
+        ProjectileEntityUtil.setVelocity(entity, x, y, z, power, uncertainty);
+    }
+
+    public static void setVelocity(CompatThrownItemEntity entity, double x, double y, double z, float power, float uncertainty) {
+        ProjectileEntityUtil.setVelocity(entity, x, y, z, power, uncertainty);
     }
 }
