8ad12fbf258fce04c86ee8a2a42943002d3ead04
PTOM76 <ptms76@outlook.jp>
2025-10-25 22:17:27 +0900

    [cp] change build priority branch to 1.20.4

--- publish.bat
+++ publish.bat
@@ -1,5 +1,17 @@
 git fetch origin
 
+git checkout 1.20.4
+timeout /T 1
+call gradlew.bat build
+call gradlew.bat publish publishMavenCommonPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenFabricPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenForgePublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenNeoForgePublicationToGitHubPackagesRepository
+call gradlew.bat curseforge
+call gradlew.bat :fabric:modrinth
+call gradlew.bat :forge:modrinth
+call gradlew.bat :neoforge:modrinth
+
 git checkout 1.16.5
 timeout /T 1
 call gradlew.bat build
@@ -39,18 +51,6 @@ call gradlew.bat publish publishMavenForgePublicationToGitHubPackagesRepository
 call gradlew.bat curseforge
 call gradlew.bat :fabric:modrinth
 call gradlew.bat :forge:modrinth
-
-git checkout 1.20.4
-timeout /T 1
-call gradlew.bat build
-call gradlew.bat publish publishMavenCommonPublicationToGitHubPackagesRepository
-call gradlew.bat publish publishMavenFabricPublicationToGitHubPackagesRepository
-call gradlew.bat publish publishMavenForgePublicationToGitHubPackagesRepository
-call gradlew.bat publish publishMavenNeoForgePublicationToGitHubPackagesRepository
-call gradlew.bat curseforge
-call gradlew.bat :fabric:modrinth
-call gradlew.bat :forge:modrinth
-call gradlew.bat :neoforge:modrinth
 
 git checkout 1.21.1
 timeout /T 1
