34553b82c11eac7ed515b0511a63d9b066d7c2ab
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2025-10-25 12:18:50 +0900

    [cp] fix getblockentitynbt

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/BlockEntityUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/BlockEntityUtil.java
@@ -19,11 +19,6 @@ public class BlockEntityUtil {
     }
 
     public static NbtCompound getBlockEntityNbt(@Nullable World world, BlockEntity blockEntity) {
-        if (world == null)
-            world = getWorld(blockEntity);
-        if (world == null)
-            return NbtUtil.create();
-
         return blockEntity.createNbt();
     }
 
