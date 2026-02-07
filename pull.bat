61b3e9ec5243348510a65d04ffe1cc59f9be41b3
PTOM76 <ptms76@outlook.jp>
2026-02-07 23:20:05 +0900

    [cp] fix publish.bat

--- pull.bat
+++ pull.bat
@@ -1,4 +1,9 @@
 git fetch origin
+git checkout 1.21.11
+timeout /T 1
+git pull
+
+git fetch origin
 git checkout 1.21.9
 timeout /T 1
 git pull
@@ -53,6 +58,6 @@ timeout /T 1
 git pull
 
 git fetch origin
-git checkout 1.21.6
+git checkout 1.21.11
 
 pause
