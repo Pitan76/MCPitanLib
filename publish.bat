2de935ac55710d248d289cee4016d8da15aeba29
PTOM76 <ptms76@outlook.jp>
2026-03-18 18:25:01 +0900

    [cp] fix publish.bat

--- publish.bat
+++ publish.bat
@@ -1,5 +1,57 @@
 git fetch origin
 
+git checkout 1.20.4
+timeout /T 1
+call gradlew.bat build
+call gradlew.bat publish publishMavenCommonPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenFabricPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenForgePublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenNeoForgePublicationToGitHubPackagesRepository
+call gradlew.bat curseforge
+call gradlew.bat :fabric:modrinth
+call gradlew.bat :forge:modrinth
+call gradlew.bat :neoforge:modrinth
+
+git checkout 1.16.5
+timeout /T 1
+call gradlew.bat build
+call gradlew.bat publish publishMavenCommonPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenFabricPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenForgePublicationToGitHubPackagesRepository
+call gradlew.bat curseforge
+call gradlew.bat :fabric:modrinth
+call gradlew.bat :forge:modrinth
+
+git checkout 1.18.2
+timeout /T 1
+call gradlew.bat build
+call gradlew.bat publish publishMavenCommonPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenFabricPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenForgePublicationToGitHubPackagesRepository
+call gradlew.bat curseforge
+call gradlew.bat :fabric:modrinth
+call gradlew.bat :forge:modrinth
+
+git checkout 1.19.2
+timeout /T 1
+call gradlew.bat build
+call gradlew.bat publish publishMavenCommonPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenFabricPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenForgePublicationToGitHubPackagesRepository
+call gradlew.bat curseforge
+call gradlew.bat :fabric:modrinth
+call gradlew.bat :forge:modrinth
+
+git checkout 1.20.1
+timeout /T 1
+call gradlew.bat build
+call gradlew.bat publish publishMavenCommonPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenFabricPublicationToGitHubPackagesRepository
+call gradlew.bat publish publishMavenForgePublicationToGitHubPackagesRepository
+call gradlew.bat curseforge
+call gradlew.bat :fabric:modrinth
+call gradlew.bat :forge:modrinth
+
 git checkout 1.21.1
 timeout /T 1
 call gradlew.bat build
