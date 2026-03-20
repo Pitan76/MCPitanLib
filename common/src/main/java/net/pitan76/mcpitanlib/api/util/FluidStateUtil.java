ad47dd45f9a6e5a7922c0c95e2908e8c52f4d153
PTOM76 <ptms76@outlook.jp>
2026-03-20 13:40:35 +0900

    [cp] feat: add FluidWrapper methods and enhance FluidStateUtil for fluid retrieval

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/FluidStateUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/FluidStateUtil.java
@@ -4,6 +4,9 @@ import net.minecraft.block.BlockState;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.tag.FluidTags;
+import net.pitan76.mcpitanlib.midohra.fluid.FluidWrapper;
+import net.pitan76.mcpitanlib.midohra.util.math.BlockPos;
+import net.pitan76.mcpitanlib.midohra.world.World;
 
 public class FluidStateUtil {
     public static boolean isWater(FluidState state) {
@@ -25,5 +28,21 @@ public class FluidStateUtil {
 
     public static Fluid getFluid(FluidState state) {
         return state.getFluid();
+    }
+
+    public static FluidWrapper getFluidWrapper(FluidState state) {
+        return FluidWrapper.of(getFluid(state));
+    }
+
+    public static FluidWrapper getFluidWrapper(BlockState state) {
+        return getFluidWrapper(getFluidState(state));
+    }
+
+    public static FluidWrapper getFluidWrapper(net.pitan76.mcpitanlib.midohra.block.BlockState state) {
+        return getFluidWrapper(state.toMinecraft());
+    }
+
+    public static FluidWrapper getFluidWrapper(World world, BlockPos pos) {
+        return getFluidWrapper(world.getBlockState(pos));
     }
 }
