a8251789b27c5ba443161c5e8e1a565517b0b78a
PTOM76 <ptms76@outlook.jp>
2026-03-20 13:39:48 +0900

    [cp] feat: add message sending methods to Player class

--- common/src/main/java/net/pitan76/mcpitanlib/api/entity/Player.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/entity/Player.java
@@ -38,6 +38,7 @@ import net.pitan76.mcpitanlib.api.sound.CompatSoundCategory;
 import net.pitan76.mcpitanlib.api.sound.CompatSoundEvent;
 import net.pitan76.mcpitanlib.api.util.CompatIdentifier;
 import net.pitan76.mcpitanlib.api.util.ScreenHandlerUtil;
+import net.pitan76.mcpitanlib.api.util.TextUtil;
 import net.pitan76.mcpitanlib.core.player.ItemCooldown;
 
 import java.util.*;
@@ -514,5 +515,17 @@ public class Player {
 
     public void eatFood(ItemStack stack, CompatFoodComponent foodComponent) {
         getEntity().getHungerManager().eat(foodComponent.build());
+    }
+
+    public void sendMessage(String message) {
+        sendMessage(TextUtil.of(message));
+    }
+
+    public void sendActionBar(String message) {
+        sendActionBar(TextUtil.of(message));
+    }
+
+    public void sendMessagef(String format, Object... args) {
+        sendMessage(TextUtil.of(String.format(format, args)));
     }
 }
