a42206da893d47c7a9593c08e47a0932d678d876
PTOM76 <ptms76@outlook.jp>
2026-03-20 13:40:35 +0900

    [cp] feat: add FluidWrapper methods and enhance FluidStateUtil for fluid retrieval

--- common/src/main/java/net/pitan76/mcpitanlib/midohra/world/IWorldView.java
+++ common/src/main/java/net/pitan76/mcpitanlib/midohra/world/IWorldView.java
@@ -5,6 +5,7 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
 import net.pitan76.mcpitanlib.midohra.block.entity.BlockEntityWrapper;
+import net.pitan76.mcpitanlib.midohra.fluid.FluidWrapper;
 
 public interface IWorldView {
     BlockEntity getBlockEntity(BlockPos pos);
@@ -19,5 +20,9 @@ public interface IWorldView {
 
     default net.pitan76.mcpitanlib.midohra.block.BlockState getBlockState(net.pitan76.mcpitanlib.midohra.util.math.BlockPos pos) {
         return net.pitan76.mcpitanlib.midohra.block.BlockState.of(getBlockState(pos.toMinecraft()));
+    }
+
+    default FluidWrapper getFluid(net.pitan76.mcpitanlib.midohra.util.math.BlockPos pos) {
+        return FluidWrapper.of(getFluidState(pos.toMinecraft()).getFluid());
     }
 }
