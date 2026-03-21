06736ce224cda15d255585fac6b322f165816efd
PTOM76 <ptms76@outlook.jp>
2026-03-21 22:55:28 +0900

    [cp] feat: enhance block entity rendering with new CompatItemRenderUtil and CompatItemDisplayContext
    # Conflicts:
    #	common/src/main/java/net/pitan76/mcpitanlib/api/client/render/block/entity/CompatBlockEntityRenderer.java
    #	common/src/main/java/net/pitan76/mcpitanlib/api/client/render/block/entity/v2/CompatBlockEntityRenderer.java

---
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/client/render/CompatItemDisplayContext.java
@@ -0,0 +1,39 @@
+package net.pitan76.mcpitanlib.api.util.client.render;
+
+import net.minecraft.item.ItemDisplayContext;
+import net.pitan76.mcpitanlib.api.util.CompatStringIdentifiable;
+
+public class CompatItemDisplayContext implements CompatStringIdentifiable {
+    private final ItemDisplayContext context;
+
+    public static final CompatItemDisplayContext NONE = of(ItemDisplayContext.NONE);
+    public static final CompatItemDisplayContext THIRD_PERSON_LEFT_HAND = of(ItemDisplayContext.THIRD_PERSON_LEFT_HAND);
+    public static final CompatItemDisplayContext THIRD_PERSON_RIGHT_HAND = of(ItemDisplayContext.THIRD_PERSON_RIGHT_HAND);
+    public static final CompatItemDisplayContext FIRST_PERSON_LEFT_HAND = of(ItemDisplayContext.FIRST_PERSON_LEFT_HAND);
+    public static final CompatItemDisplayContext FIRST_PERSON_RIGHT_HAND = of(ItemDisplayContext.FIRST_PERSON_RIGHT_HAND);
+    public static final CompatItemDisplayContext HEAD = of(ItemDisplayContext.HEAD);
+    public static final CompatItemDisplayContext GUI = of(ItemDisplayContext.GUI);
+    public static final CompatItemDisplayContext GROUND = of(ItemDisplayContext.GROUND);
+    public static final CompatItemDisplayContext FIXED = of(ItemDisplayContext.FIXED);
+
+    public CompatItemDisplayContext(ItemDisplayContext context) {
+        this.context = context;
+    }
+
+    public static CompatItemDisplayContext of(ItemDisplayContext context) {
+        return new CompatItemDisplayContext(context);
+    }
+
+    public ItemDisplayContext getContext() {
+        return context;
+    }
+
+    public String getName() {
+        return context.name();
+    }
+
+    @Override
+    public String asString_compat() {
+        return getName();
+    }
+}
