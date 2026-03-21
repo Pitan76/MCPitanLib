413d0547b9196846bb81ce4cd07cacaaf0309014
PTOM76 <ptms76@outlook.jp>
2026-03-21 22:55:28 +0900

    [cp] feat: enhance block entity rendering with new CompatItemRenderUtil and CompatItemDisplayContext
    # Conflicts:
    #	common/src/main/java/net/pitan76/mcpitanlib/api/client/render/block/entity/CompatBlockEntityRenderer.java
    #	common/src/main/java/net/pitan76/mcpitanlib/api/client/render/block/entity/v2/CompatBlockEntityRenderer.java
    
    # Conflicts:
    #	common/src/main/java/net/pitan76/mcpitanlib/api/client/render/block/entity/CompatBlockEntityRenderer.java

---
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/client/render/CompatItemDisplayContext.java
@@ -0,0 +1,39 @@
+package net.pitan76.mcpitanlib.api.util.client.render;
+
+import net.minecraft.item.ModelTransformationMode;
+import net.pitan76.mcpitanlib.api.util.CompatStringIdentifiable;
+
+public class CompatItemDisplayContext implements CompatStringIdentifiable {
+    private final ModelTransformationMode context;
+
+    public static final CompatItemDisplayContext NONE = of(ModelTransformationMode.NONE);
+    public static final CompatItemDisplayContext THIRD_PERSON_LEFT_HAND = of(ModelTransformationMode.THIRD_PERSON_LEFT_HAND);
+    public static final CompatItemDisplayContext THIRD_PERSON_RIGHT_HAND = of(ModelTransformationMode.THIRD_PERSON_RIGHT_HAND);
+    public static final CompatItemDisplayContext FIRST_PERSON_LEFT_HAND = of(ModelTransformationMode.FIRST_PERSON_LEFT_HAND);
+    public static final CompatItemDisplayContext FIRST_PERSON_RIGHT_HAND = of(ModelTransformationMode.FIRST_PERSON_RIGHT_HAND);
+    public static final CompatItemDisplayContext HEAD = of(ModelTransformationMode.HEAD);
+    public static final CompatItemDisplayContext GUI = of(ModelTransformationMode.GUI);
+    public static final CompatItemDisplayContext GROUND = of(ModelTransformationMode.GROUND);
+    public static final CompatItemDisplayContext FIXED = of(ModelTransformationMode.FIXED);
+
+    public CompatItemDisplayContext(ModelTransformationMode context) {
+        this.context = context;
+    }
+
+    public static CompatItemDisplayContext of(ModelTransformationMode context) {
+        return new CompatItemDisplayContext(context);
+    }
+
+    public ModelTransformationMode getContext() {
+        return context;
+    }
+
+    public String getName() {
+        return context.name();
+    }
+
+    @Override
+    public String asString_compat() {
+        return getName();
+    }
+}
