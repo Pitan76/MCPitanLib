45ca1e5fe5dce782a4a570841b8b1702e3fd80f2
PTOM76 <ptms76@outlook.jp>
2026-03-24 17:11:19 +0900

    [cp] feat: add utility methods for BlockPos, ItemStack, and NbtCompound

--- common/src/main/java/net/pitan76/mcpitanlib/api/event/nbt/WriteNbtArgs.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/event/nbt/WriteNbtArgs.java
@@ -28,4 +28,12 @@ public class WriteNbtArgs extends NbtRWArgs {
     public NbtRWArgs copy() {
         return new WriteNbtArgs(nbt.copy(), registryLookup);
     }
+
+    public WriteNbtArgs(net.pitan76.mcpitanlib.midohra.nbt.NbtCompound nbt, CompatRegistryLookup registryLookup) {
+        this(nbt.toMinecraft(), registryLookup);
+    }
+
+    public WriteNbtArgs(net.pitan76.mcpitanlib.midohra.nbt.NbtCompound nbt) {
+        this(nbt.toMinecraft());
+    }
 }
