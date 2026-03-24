1cf638658fb960652b644361bb51193f5010952e
PTOM76 <ptms76@outlook.jp>
2026-03-24 22:41:31 +0900

    [cp] feat: add EntityWrapper and ItemEntityWrapper classes with utility methods for entity manipulation

--- common/src/main/java/net/pitan76/mcpitanlib/midohra/block/entity/BlockEntityWrapper.java
+++ common/src/main/java/net/pitan76/mcpitanlib/midohra/block/entity/BlockEntityWrapper.java
@@ -1,13 +1,17 @@
 package net.pitan76.mcpitanlib.midohra.block.entity;
 
 import net.pitan76.mcpitanlib.api.registry.CompatRegistryLookup;
+import net.pitan76.mcpitanlib.api.tile.CompatBlockEntity;
 import net.pitan76.mcpitanlib.api.util.BlockEntityUtil;
 import net.pitan76.mcpitanlib.api.util.RegistryLookupUtil;
 import net.pitan76.mcpitanlib.midohra.block.BlockState;
 import net.pitan76.mcpitanlib.midohra.block.BlockWrapper;
+import net.pitan76.mcpitanlib.midohra.easybuilder.built.BuiltBlockEntity;
 import net.pitan76.mcpitanlib.midohra.nbt.NbtCompound;
 import net.pitan76.mcpitanlib.midohra.util.math.BlockPos;
 import net.pitan76.mcpitanlib.midohra.world.World;
+
+import java.util.Optional;
 
 public class BlockEntityWrapper {
     private final net.minecraft.block.entity.BlockEntity blockEntity;
@@ -93,5 +97,37 @@ public class BlockEntityWrapper {
 
     public void readNbt(NbtCompound nbt) {
         readNbt(nbt, RegistryLookupUtil.getRegistryLookup(get()));
+    }
+
+    @Override
+    public int hashCode() {
+        return get() != null ? get().hashCode() : 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        BlockEntityWrapper blockEntity = (BlockEntityWrapper) obj;
+        return get() == blockEntity.get();
+    }
+
+    public static BlockEntityWrapper of(BlockPos pos, World world) {
+        net.minecraft.block.entity.BlockEntity blockEntity = BlockEntityUtil.getBlockEntity(world.toMinecraft(), pos.toMinecraft());
+        return of(blockEntity);
+    }
+
+    public Optional<CompatBlockEntity> toCompatBlockEntity() {
+        if (get() instanceof CompatBlockEntity) {
+            return Optional.of((CompatBlockEntity) get());
+        }
+        return Optional.empty();
+    }
+
+    public Optional<BuiltBlockEntity> toBuiltBlockEntity() {
+        if (get() instanceof BuiltBlockEntity) {
+            return Optional.of((BuiltBlockEntity) get());
+        }
+        return Optional.empty();
     }
 }
