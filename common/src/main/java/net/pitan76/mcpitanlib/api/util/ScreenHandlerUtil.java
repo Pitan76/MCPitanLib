28c887fc37efca05263a808d5872732a12d2e402
PTOM76 <ptms76@outlook.jp>
2026-04-01 23:17:50 +0900

    [cp] feat: add cursor stack handling methods in ScreenHandlerUtil and SimpleScreenHandler

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/ScreenHandlerUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/ScreenHandlerUtil.java
@@ -2,6 +2,7 @@ package net.pitan76.mcpitanlib.api.util;
 
 import dev.architectury.registry.menu.ExtendedMenuProvider;
 import dev.architectury.registry.menu.MenuRegistry;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
@@ -50,5 +51,21 @@ public class ScreenHandlerUtil {
 
     public static ScreenHandlerType<?> fromIndex(int index) {
         return Registry.SCREEN_HANDLER.get(index);
+    }
+
+    public static ItemStack getCursorStack(ScreenHandler screenHandler) {
+        return screenHandler.getCursorStack();
+    }
+
+    public static net.pitan76.mcpitanlib.midohra.item.ItemStack getCursorStackM(ScreenHandler screenHandler) {
+        return net.pitan76.mcpitanlib.midohra.item.ItemStack.of(getCursorStack(screenHandler));
+    }
+
+    public static void setCursorStack(ScreenHandler screenHandler, ItemStack stack) {
+        screenHandler.setCursorStack(stack);
+    }
+
+    public static void setCursorStackM(ScreenHandler screenHandler, net.pitan76.mcpitanlib.midohra.item.ItemStack stack) {
+        setCursorStack(screenHandler, stack.toMinecraft());
     }
 }
