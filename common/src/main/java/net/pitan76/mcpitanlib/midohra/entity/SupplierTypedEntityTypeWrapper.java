aa5a5f35d080814bbe69033fd49fda56dbd65273
PTOM76 <ptms76@outlook.jp>
2026-04-05 16:09:08 +0900

    [cp] feat: add new item and block wrappers, enhance existing utility classes with additional methods

---
+++ common/src/main/java/net/pitan76/mcpitanlib/midohra/entity/SupplierTypedEntityTypeWrapper.java
@@ -0,0 +1,42 @@
+package net.pitan76.mcpitanlib.midohra.entity;
+
+import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.pitan76.mcpitanlib.api.registry.result.RegistryResult;
+
+import java.util.function.Supplier;
+
+public class SupplierTypedEntityTypeWrapper<T extends Entity> extends TypedEntityTypeWrapper<T> {
+    private final Supplier<EntityType<T>> supplier;
+
+    protected SupplierTypedEntityTypeWrapper(Supplier<EntityType<T>> supplier) {
+        super(null);
+        this.supplier = supplier;
+    }
+
+    public static <T extends Entity> SupplierTypedEntityTypeWrapper<T> of(Supplier<EntityType<T>> supplier) {
+        return new SupplierTypedEntityTypeWrapper<>(supplier);
+    }
+
+    public static <T extends Entity> SupplierTypedEntityTypeWrapper<T> of(RegistryResult<EntityType<T>> result) {
+        return new SupplierTypedEntityTypeWrapper<>(result::get);
+    }
+
+    public static <T extends Entity> SupplierTypedEntityTypeWrapper<T> of(RegistrySupplier<EntityType<T>> result) {
+        return new SupplierTypedEntityTypeWrapper<>(result::get);
+    }
+
+    public static <T extends Entity> SupplierTypedEntityTypeWrapper<T> of(SupplierEntityTypeWrapper result) {
+        return new SupplierTypedEntityTypeWrapper<>(() -> (EntityType<T>) result.get());
+    }
+
+    public SupplierEntityTypeWrapper asNonTyped() {
+        return SupplierEntityTypeWrapper.of(supplier::get);
+    }
+
+    @Override
+    public EntityType<T> get() {
+        return supplier.get();
+    }
+}
