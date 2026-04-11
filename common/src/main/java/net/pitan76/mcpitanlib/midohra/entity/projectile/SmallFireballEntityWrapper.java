b7fc709c3b8a7d84514dd663795b6f557e18a1c1
PTOM76 <ptms76@outlook.jp>
2026-04-11 22:03:19 +0900

    [cp] feat: add wrappers for various projectile entities and enhance entity handling

---
+++ common/src/main/java/net/pitan76/mcpitanlib/midohra/entity/projectile/SmallFireballEntityWrapper.java
@@ -0,0 +1,50 @@
+package net.pitan76.mcpitanlib.midohra.entity.projectile;
+
+import net.minecraft.entity.projectile.SmallFireballEntity;
+import net.pitan76.mcpitanlib.api.util.entity.SmallFireballEntityUtil;
+import net.pitan76.mcpitanlib.midohra.entity.EntityWrapper;
+import net.pitan76.mcpitanlib.midohra.item.ItemStack;
+import net.pitan76.mcpitanlib.midohra.util.math.Vector3d;
+import net.pitan76.mcpitanlib.midohra.world.World;
+
+public class SmallFireballEntityWrapper extends ProjectileEntityWrapper<SmallFireballEntity> {
+    protected SmallFireballEntityWrapper(SmallFireballEntity entity) {
+        super(entity);
+    }
+
+    public static SmallFireballEntityWrapper ofRaw(SmallFireballEntity entity) {
+        return new SmallFireballEntityWrapper(entity);
+    }
+
+    public static SmallFireballEntityWrapper ofSmallFireball(EntityWrapper wrapper) {
+        return new SmallFireballEntityWrapper((SmallFireballEntity) wrapper.get());
+    }
+
+    public static boolean isSmallFireball(EntityWrapper wrapper) {
+        return wrapper.get() instanceof SmallFireballEntity;
+    }
+
+    public static SmallFireballEntityWrapper create(World world, double x, double y, double z, double velocityX, double velocityY, double velocityZ) {
+        return new SmallFireballEntityWrapper(SmallFireballEntityUtil.create(world.toMinecraft(), x, y, z, velocityX, velocityY, velocityZ));
+    }
+
+    public static SmallFireballEntityWrapper create(World world, Vector3d pos, Vector3d velocity) {
+        return create(world, pos.x, pos.y, pos.z, velocity.x, velocity.y, velocity.z);
+    }
+
+    public void setStack(ItemStack stack) {
+        SmallFireballEntityUtil.setItem(get(), stack.toMinecraft());
+    }
+
+    public ItemStack getStack() {
+        return ItemStack.of(SmallFireballEntityUtil.getItem(get()));
+    }
+
+    public void setVelocity(double x, double y, double z, float velocity, float divergence) {
+        SmallFireballEntityUtil.setVelocity(get(), x, y, z, velocity, divergence);
+    }
+
+    public void setVelocity(Vector3d velocity, float speed, float uncertainty) {
+        setVelocity(velocity.x, velocity.y, velocity.z, speed, uncertainty);
+    }
+}
