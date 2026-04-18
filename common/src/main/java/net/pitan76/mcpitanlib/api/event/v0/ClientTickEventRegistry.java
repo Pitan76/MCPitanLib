9d867ff936a4f616a611f3863cc565a8486975d4
PTOM76 <ptms76@outlook.jp>
2026-04-18 19:27:30 +0900

    fix: implement ClientTickEventRegistry for NeoForge and add event registration methods

--- common/src/main/java/net/pitan76/mcpitanlib/api/event/v0/ClientTickEventRegistry.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/event/v0/ClientTickEventRegistry.java
@@ -1,25 +1,29 @@
 package net.pitan76.mcpitanlib.api.event.v0;
 
+import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.Minecraft;
 
 public class ClientTickEventRegistry {
+    @ExpectPlatform
     public static void registerPost(Client client) {
-        ClientTickEvents.END_CLIENT_TICK.register(client::tick);
+
     }
 
+    @ExpectPlatform
     public static void registerPre(Client client) {
-        ClientTickEvents.START_CLIENT_TICK.register(client::tick);
+
     }
 
+    @ExpectPlatform
     public static void registerLevelPost(ClientLevel world) {
-        ClientTickEvents.END_LEVEL_TICK.register(world::tick);
+
     }
 
+    @ExpectPlatform
     public static void registerLevelPre(ClientLevel world) {
-        ClientTickEvents.START_LEVEL_TICK.register(world::tick);
+
     }
 
     @Environment(EnvType.CLIENT)
---
+++ neoforge/src/main/java/net/pitan76/mcpitanlib/api/event/v0/neoforge/ClientTickEventRegistryImpl.java
@@ -0,0 +1,73 @@
+package net.pitan76.mcpitanlib.api.event.v0.neoforge;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.event.tick.LevelTickEvent;
+import net.pitan76.mcpitanlib.api.event.v0.ClientTickEventRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@EventBusSubscriber(modid = "mcpitanlib", value = Dist.CLIENT)
+public class ClientTickEventRegistryImpl {
+
+    private static final List<ClientTickEventRegistry.Client> preClients = new ArrayList<>();
+    private static final List<ClientTickEventRegistry.Client> postClients = new ArrayList<>();
+    private static final List<ClientTickEventRegistry.ClientLevel> preLevels = new ArrayList<>();
+    private static final List<ClientTickEventRegistry.ClientLevel> postLevels = new ArrayList<>();
+
+    public static void registerPre(ClientTickEventRegistry.Client client) {
+        preClients.add(client);
+    }
+
+    public static void registerPost(ClientTickEventRegistry.Client client) {
+        postClients.add(client);
+    }
+
+    public static void registerLevelPre(ClientTickEventRegistry.ClientLevel world) {
+        preLevels.add(world);
+    }
+
+    public static void registerLevelPost(ClientTickEventRegistry.ClientLevel world) {
+        postLevels.add(world);
+    }
+
+    @SubscribeEvent
+    public static void onClientTickPre(ClientTickEvent.Pre event) {
+        Minecraft mc = Minecraft.getInstance();
+        for (ClientTickEventRegistry.Client listener : preClients) {
+            listener.tick(mc);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onClientTickPost(ClientTickEvent.Post event) {
+        Minecraft mc = Minecraft.getInstance();
+        for (ClientTickEventRegistry.Client listener : postClients) {
+            listener.tick(mc);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLevelTickPre(LevelTickEvent.Pre event) {
+        // LevelTickEventはサーバー側のワールドでも発火するため、クライアントワールドかどうかの判定が必須
+        if (event.getLevel() instanceof ClientLevel clientLevel) {
+            for (ClientTickEventRegistry.ClientLevel listener : preLevels) {
+                listener.tick(clientLevel);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLevelTickPost(LevelTickEvent.Post event) {
+        if (event.getLevel() instanceof ClientLevel clientLevel) {
+            for (ClientTickEventRegistry.ClientLevel listener : postLevels) {
+                listener.tick(clientLevel);
+            }
+        }
+    }
+}
