7ef94d80ccb113221ab715902659a4e8c15449ae
PTOM76 <ptms76@outlook.jp>
2026-08-17 12:12:16 +0900

    fix: persistent state

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/PersistentStateUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/PersistentStateUtil.java
@@ -13,32 +13,51 @@ import net.pitan76.mcpitanlib.api.event.nbt.ReadNbtArgs;
 import net.pitan76.mcpitanlib.api.event.nbt.WriteNbtArgs;
 import net.pitan76.mcpitanlib.api.world.CompatiblePersistentState;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class PersistentStateUtil {
+    private static final Map<String, SavedDataType<?>> TYPE_CACHE = new ConcurrentHashMap<>();
+
+    @SuppressWarnings("unchecked")
     public static <T extends SavedData> T getOrCreate(SavedDataStorage manager, String id, Supplier<T> supplier, Function<CompoundTag, T> function) {
+        SavedDataType<T> type = (SavedDataType<T>) TYPE_CACHE.computeIfAbsent(id, key -> createType(key, supplier, function));
+        return manager.computeIfAbsent(type);
+    }
+
+    private static <T extends SavedData> SavedDataType<T> createType(String id, Supplier<T> supplier, Function<CompoundTag, T> function) {
         Codec<T> codec = CompoundTag.CODEC.xmap(
                 // NBT -> PersistentState
                 (nbt) -> {
-                    T state = supplier.get();
+                    T state = function != null ? function.apply(nbt) : null;
 
-                    if (state instanceof CompatiblePersistentState)
-                        ((CompatiblePersistentState) state).readNbt(new ReadNbtArgs(nbt));
+                    if (state == null) {
+                        state = supplier.get();
+
+                        if (state instanceof CompatiblePersistentState)
+                            ((CompatiblePersistentState) state).readNbt(new ReadNbtArgs(nbt));
+                    }
+
 
                     return state;
                 },
                 // PersistentState -> NBT
                 (state) -> {
-                    if (state instanceof CompatiblePersistentState)
-                        return ((CompatiblePersistentState) state).writeNbt(new WriteNbtArgs(new CompoundTag()));
+                    CompoundTag tag;
 
-                    return NbtUtil.create();
+                    if (state instanceof CompatiblePersistentState)
+                        tag = ((CompatiblePersistentState) state).writeNbt(new WriteNbtArgs(new CompoundTag()));
+                    else
+                        tag = NbtUtil.create();
+
+
+                    return tag;
                 }
         );
 
-        SavedDataType<T> type = new SavedDataType<>(IdentifierUtil.id("mcpitanlib", id), supplier, codec, DataFixTypes.LEVEL);
-        return manager.computeIfAbsent(type);
+        return new SavedDataType<>(IdentifierUtil.id("mcpitanlib", id), supplier, codec, DataFixTypes.LEVEL);
     }
 
     public static SavedDataStorage getManagerFromServer(MinecraftServer server) {
