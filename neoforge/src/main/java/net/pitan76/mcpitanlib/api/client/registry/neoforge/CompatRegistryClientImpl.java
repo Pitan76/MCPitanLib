151a16781e46717d07234996d31f18451d262109
PTOM76 <ptms76@outlook.jp>
2026-08-18 16:26:16 +0900

    CompatRegistryClient 1219 neoforge

--- neoforge/src/main/java/net/pitan76/mcpitanlib/api/client/registry/neoforge/CompatRegistryClientImpl.java
+++ neoforge/src/main/java/net/pitan76/mcpitanlib/api/client/registry/neoforge/CompatRegistryClientImpl.java
@@ -10,7 +10,7 @@ import net.minecraft.client.model.TexturedModelData;
 import net.minecraft.client.particle.ParticleFactory;
 import net.minecraft.client.particle.SpriteProvider;
 import net.minecraft.client.render.BlockRenderLayer;
-import net.minecraft.client.render.BlockRenderLayers;
+import net.minecraft.client.render.RenderLayers;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import net.minecraft.client.render.block.entity.state.BlockEntityRenderState;
 import net.minecraft.client.render.entity.EntityRendererFactory;
@@ -172,10 +172,10 @@ public class CompatRegistryClientImpl {
     // ---- RenderLayer ----
 
     public static void registerRenderLayerBlock(BlockRenderLayer layer, Block block) {
-        BlockRenderLayers.setRenderLayer(block, layer);
+        RenderLayers.setRenderLayer(block, layer);
     }
 
     public static void registerRenderLayerFluid(BlockRenderLayer layer, Fluid fluid) {
-        BlockRenderLayers.setRenderLayer(fluid, layer);
+        RenderLayers.setRenderLayer(fluid, layer);
     }
 }
