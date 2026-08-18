2447a6c1fa94e0bf3b8c69fb92c212c6560afd70
PTOM76 <ptms76@outlook.jp>
2026-08-18 16:54:05 +0900

    fix: share payload type registration between client and server (fabric)

--- fabric/src/main/java/net/pitan76/mcpitanlib/core/network/fabric/ServerPlayNetworkingImpl.java
+++ fabric/src/main/java/net/pitan76/mcpitanlib/core/network/fabric/ServerPlayNetworkingImpl.java
@@ -1,7 +1,6 @@
 package net.pitan76.mcpitanlib.core.network.fabric;
 
 import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.CustomPayload;
@@ -10,8 +9,6 @@ import net.minecraft.util.Identifier;
 import net.pitan76.mcpitanlib.api.network.ServerNetworking;
 import net.pitan76.mcpitanlib.core.network.BufPayload;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class ServerPlayNetworkingImpl {
 
@@ -20,7 +17,7 @@ public class ServerPlayNetworkingImpl {
     }
 
     public static void registerGlobalReceiver(CustomPayload.Id<BufPayload> id, ServerNetworking.ServerNetworkHandler handler) {
-        registerC2SPayloadType(id.id());
+        FabricPayloadTypes.registerC2S(id.id());
 
         ServerPlayNetworking.registerGlobalReceiver(id, (payload, context) -> {
             PacketByteBuf buf = new PacketByteBuf(Unpooled.wrappedBuffer(payload.getData()));
@@ -29,22 +26,7 @@ public class ServerPlayNetworkingImpl {
         });
     }
 
-    private static final List<Identifier> s2cRegisteredList = new ArrayList<>();
-    private static final List<Identifier> c2sRegisteredList = new ArrayList<>();
-
     public static void registerS2CPayloadType(Identifier identifier) {
-        if (s2cRegisteredList.contains(identifier)) return;
-        s2cRegisteredList.add(identifier);
-
-        BufPayload.Id<BufPayload> id = BufPayload.id(identifier);
-        PayloadTypeRegistry.playS2C().register(id, BufPayload.getCodec(id));
-    }
-
-    private static void registerC2SPayloadType(Identifier identifier) {
-        if (c2sRegisteredList.contains(identifier)) return;
-        c2sRegisteredList.add(identifier);
-
-        BufPayload.Id<BufPayload> id = BufPayload.id(identifier);
-        PayloadTypeRegistry.playC2S().register(id, BufPayload.getCodec(id));
+        FabricPayloadTypes.registerS2C(identifier);
     }
 }
