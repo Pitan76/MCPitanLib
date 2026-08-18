afc089e22b4c759891bcd919f9b2049a16be03c3
PTOM76 <ptms76@outlook.jp>
2026-08-18 14:26:01 +0900

    fix: merge register fuel map

--- common/src/main/java/net/pitan76/mcpitanlib/api/registry/FuelRegistry.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/registry/FuelRegistry.java
@@ -11,10 +11,8 @@ public class FuelRegistry {
     private static final Map<String, Map<Supplier<ItemLike>, Integer>> FUEL_MAP = new HashMap<>();
 
     public static void register(Supplier<ItemLike> itemSupplier, int time, String namespace) {
-        Map<Supplier<ItemLike>, Integer> map = new HashMap<>();
-        map.put(itemSupplier, time);
-
-        FUEL_MAP.put(namespace, map);
+        // 同じnamespaceに複数登録できるようマージする
+        FUEL_MAP.computeIfAbsent(namespace, k -> new HashMap<>()).put(itemSupplier, time);
     }
 
     @Deprecated
