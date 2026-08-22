f655c1ba558b02a704f5c24bfa562c07d8d04784
PTOM76 <admin@pitan76.net>
2026-08-22 20:26:34 +0900

    [cp] null check factory3

--- common/src/main/java/net/pitan76/mcpitanlib/api/gui/ExtendedScreenHandlerTypeBuilder.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/gui/ExtendedScreenHandlerTypeBuilder.java
@@ -58,6 +58,8 @@ public class ExtendedScreenHandlerTypeBuilder<T extends ScreenHandler> {
 
         @Override
         default T create(CreateMenuEvent e, PacketByteBuf buf) {
+            if (buf == null) return create(e, (net.pitan76.mcpitanlib.midohra.network.PacketByteBuf) null);
+
             return create(e, net.pitan76.mcpitanlib.midohra.network.PacketByteBuf.of(buf));
         }
     }
