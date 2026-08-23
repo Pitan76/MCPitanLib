56b166029b147646b1b28ae7e95a066b42b9fbeb
PTOM76 <admin@pitan76.net>
2026-08-23 13:33:44 +0900

    [cp] chunk section api, fix render

--- common/src/main/java/net/pitan76/mcpitanlib/api/client/render/block/entity/CompatBlockEntityRenderer.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/render/block/entity/CompatBlockEntityRenderer.java
@@ -4,6 +4,8 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.block.entity.BlockEntity;
+import net.pitan76.mcpitanlib.midohra.util.math.Box;
 import net.pitan76.mcpitanlib.api.client.render.block.entity.event.BlockEntityRenderEvent;
 import net.pitan76.mcpitanlib.api.tile.CompatBlockEntity;
 
@@ -15,6 +17,30 @@ public interface CompatBlockEntityRenderer<T extends CompatBlockEntity> extends 
     default void render(T entity, float tickProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, Vec3d cameraPos) {
         render(new BlockEntityRenderEvent<>(this, entity, tickProgress, matrices, vertexConsumers, light, overlay));
     }
+
+    /**
+     * カリングに使う描画範囲。BlockEntityより大きい範囲を描画するときに実際の範囲を返す。
+     * NeoForge専用 (バニラ/Fabricは描画範囲でカリングしない)。
+     * @return nullならrendersOutsideBoundingBoxOverrideに従う
+     */
+    default Box getRenderBoundingBoxOverride(T blockEntity) {
+        return null;
+    }
+
+    // NeoForgeのIBlockEntityRendererExtension#getRenderBoundingBoxを実行時にオーバーライドする。
+    // 引数がTでなく生BlockEntityなのは、消去後のシグネチャを一致させないとオーバーライドにならないため。
+    @SuppressWarnings("unchecked")
+    default net.minecraft.util.math.Box getRenderBoundingBox(BlockEntity blockEntity) {
+        Box box = getRenderBoundingBoxOverride((T) blockEntity);
+        if (box != null) return box.toMinecraft();
+
+        if (rendersOutsideBoundingBoxOverride((T) blockEntity)) return INFINITE_RENDER_BOX;
+
+        return new net.minecraft.util.math.Box(blockEntity.getPos());
+    }
+
+    // カリングを実質無効にする巨大なAABB
+    net.minecraft.util.math.Box INFINITE_RENDER_BOX = new net.minecraft.util.math.Box(-3.0E7D, -3.0E7D, -3.0E7D, 3.0E7D, 3.0E7D, 3.0E7D);
 
     default boolean rendersOutsideBoundingBoxOverride(T blockEntity) {
         return BlockEntityRenderer.super.rendersOutsideBoundingBox();
