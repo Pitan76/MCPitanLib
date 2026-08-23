11bd11f34702cbe13edd826c4d3802d9ff4b69bb
PTOM76 <admin@pitan76.net>
2026-08-23 13:35:31 +0900

    [cp] add Item/FluidLookup

--- common/src/main/java/net/pitan76/mcpitanlib/api/transfer/fluid/v1/FluidStorageUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/transfer/fluid/v1/FluidStorageUtil.java
@@ -1,7 +1,15 @@
 package net.pitan76.mcpitanlib.api.transfer.fluid.v1;
 
 import dev.architectury.injectables.annotations.ExpectPlatform;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.fluid.Fluid;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.BiFunction;
 
 public class FluidStorageUtil {
     @ExpectPlatform
@@ -32,5 +40,36 @@ public class FluidStorageUtil {
      */
     public static long buckets(double buckets) {
         return (long) (bucketAmount() * buckets);
+    }
+
+    /**
+     * 指定した位置の流体タンクを取得する。他MODの機械も対象になる。
+     * @param side 面 (nullで面を指定しない)
+     * @return 流体タンク (存在しない場合はnull)
+     */
+    @ExpectPlatform
+    @Nullable
+    public static IFluidHandler getFluidHandler(World world, BlockPos pos, @Nullable Direction side) {
+        throw new AssertionError();
+    }
+
+    /**
+     * BlockEntityTypeに流体タンクを紐付ける。
+     * 渡せるのは {@link #withFixedCapacity} で作ったタンクのみ。
+     */
+    @ExpectPlatform
+    public static void registerFluidStorage(BlockEntityType<?> type, BiFunction<BlockEntity, Direction, IFluidStorage> provider) {
+        throw new AssertionError();
+    }
+
+    @Nullable
+    public static IFluidHandler getFluidHandler(World world, BlockPos pos) {
+        return getFluidHandler(world, pos, null);
+    }
+
+    @Nullable
+    public static IFluidHandler getFluidHandler(BlockEntity blockEntity, @Nullable Direction side) {
+        if (blockEntity == null || blockEntity.getWorld() == null) return null;
+        return getFluidHandler(blockEntity.getWorld(), blockEntity.getPos(), side);
     }
 }
--- common/src/main/java/net/pitan76/mcpitanlib/api/transfer/fluid/v1/IFluidStorage.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/transfer/fluid/v1/IFluidStorage.java
@@ -1,14 +1,12 @@
 package net.pitan76.mcpitanlib.api.transfer.fluid.v1;
 
-import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.nbt.NbtCompound;
 import net.pitan76.mcpitanlib.api.event.nbt.ReadNbtArgs;
 import net.pitan76.mcpitanlib.api.event.nbt.WriteNbtArgs;
 import net.pitan76.mcpitanlib.api.registry.CompatRegistryLookup;
-import net.pitan76.mcpitanlib.midohra.fluid.FluidWrapper;
 
-public interface IFluidStorage {
+public interface IFluidStorage extends IFluidHandler {
     long getAmount();
 
     long getCapacity();
@@ -17,40 +15,12 @@ public interface IFluidStorage {
 
     void setResource(IFluidVariant variant);
 
-    long insert(IFluidVariant variant, long maxAmount, boolean simulate);
-
-    long extract(IFluidVariant variant, long maxAmount, boolean simulate);
-
     void writeNbt(WriteNbtArgs args);
 
     void readNbt(ReadNbtArgs args);
 
     default void readNbt(WriteNbtArgs args) {
         readNbt(new ReadNbtArgs(args.getNbt(), args.getWrapperLookup()));
-    }
-
-    default long insert(IFluidVariant variant, long maxAmount) {
-        return insert(variant, maxAmount, false);
-    }
-
-    default long extract(IFluidVariant variant, long maxAmount) {
-        return extract(variant, maxAmount, false);
-    }
-
-    default long insert(Fluid fluid, long maxAmount) {
-        return insert(FluidStorageUtil.getVariant(fluid), maxAmount);
-    }
-
-    default long extract(Fluid fluid, long maxAmount) {
-        return extract(FluidStorageUtil.getVariant(fluid), maxAmount);
-    }
-
-    default long insert(FluidWrapper wrapper, long maxAmount) {
-        return insert(wrapper.get(), maxAmount);
-    }
-
-    default long extract(FluidWrapper wrapper, long maxAmount) {
-        return extract(wrapper.get(), maxAmount);
     }
 
     default boolean isResourceBlank() {
@@ -67,30 +37,6 @@ public interface IFluidStorage {
 
     default boolean isFull() {
         return getAmount() >= getCapacity();
-    }
-
-    default boolean canInsert(IFluidVariant variant, long amount) {
-        return insert(variant, amount, true) == amount;
-    }
-
-    default boolean canExtract(IFluidVariant variant, long amount) {
-        return extract(variant, amount, true) == amount;
-    }
-
-    default boolean canInsert(Fluid fluid, long amount) {
-        return canInsert(FluidStorageUtil.getVariant(fluid), amount);
-    }
-
-    default boolean canExtract(Fluid fluid, long amount) {
-        return canExtract(FluidStorageUtil.getVariant(fluid), amount);
-    }
-
-    default boolean canInsert(FluidWrapper wrapper, long amount) {
-        return canInsert(wrapper.get(), amount);
-    }
-
-    default boolean canExtract(FluidWrapper wrapper, long amount) {
-        return canExtract(wrapper.get(), amount);
     }
 
     default void writeNbt(NbtCompound nbt, CompatRegistryLookup registryLookup) {
