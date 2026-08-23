435df49d6e0f0400925f46b06d3d733fa9751bd9
PTOM76 <mizuto1127no79@gmail.com>
2026-08-23 13:16:59 +0900

    [cp] chunk section api, fix render

--- common/src/main/java/net/pitan76/mcpitanlib/api/client/render/block/entity/CompatBlockEntityRenderer.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/render/block/entity/CompatBlockEntityRenderer.java
@@ -7,6 +7,8 @@ import net.minecraft.client.render.command.OrderedRenderCommandQueue;
 import net.minecraft.client.render.state.CameraRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.block.entity.BlockEntity;
+import net.pitan76.mcpitanlib.midohra.util.math.Box;
 import net.pitan76.mcpitanlib.api.client.render.block.entity.event.BlockEntityRenderEvent;
 import net.pitan76.mcpitanlib.api.tile.CompatBlockEntity;
 
@@ -22,6 +24,30 @@ public interface CompatBlockEntityRenderer<T extends CompatBlockEntity, S extend
     default void render(S state, MatrixStack matrices, OrderedRenderCommandQueue queue, CameraRenderState cameraState) {
         render(new BlockEntityRenderEvent<>(this, state, matrices, queue, cameraState));
     }
+
+    /**
+     * カリングに使う描画範囲。BlockEntityより大きい範囲を描画するときに実際の範囲を返す。
+     * NeoForge専用 (バニラ/Fabricは描画範囲でカリングしない)。
+     * @return nullならrendersOutsideBoundingBoxOverrideに従う
+     */
+    default Box getRenderBoundingBoxOverride(T blockEntity) {
+        return null;
+    }
+
+    // NeoForgeのIBlockEntityRendererExtension#getRenderBoundingBoxを実行時にオーバーライドする。
+    // 引数がTでなく生BlockEntityなのは、消去後のシグネチャを一致させないとオーバーライドにならないため。
+    @SuppressWarnings("unchecked")
+    default net.minecraft.util.math.Box getRenderBoundingBox(BlockEntity blockEntity) {
+        Box box = getRenderBoundingBoxOverride((T) blockEntity);
+        if (box != null) return box.toMinecraft();
+
+        if (rendersOutsideBoundingBoxOverride((T) blockEntity)) return INFINITE_RENDER_BOX;
+
+        return new net.minecraft.util.math.Box(blockEntity.getPos());
+    }
+
+    // カリングを実質無効にする巨大なAABB
+    net.minecraft.util.math.Box INFINITE_RENDER_BOX = new net.minecraft.util.math.Box(-3.0E7D, -3.0E7D, -3.0E7D, 3.0E7D, 3.0E7D, 3.0E7D);
 
     default boolean rendersOutsideBoundingBoxOverride(T blockEntity) {
         return BlockEntityRenderer.super.rendersOutsideBoundingBox();
