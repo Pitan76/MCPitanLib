a72de1ae8bc272dae247f273c0b5433c862d75cf
PTOM76 <admin@pitan76.net>
2026-08-24 13:33:26 +0900

    [cp] fix tool material level

--- common/src/main/java/net/pitan76/mcpitanlib/api/item/tool/CompatibleToolMaterial.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/item/tool/CompatibleToolMaterial.java
@@ -4,7 +4,6 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.recipe.Ingredient;
-import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.registry.tag.TagKey;
 import net.pitan76.mcpitanlib.api.util.IngredientUtil;
@@ -36,23 +35,7 @@ public interface CompatibleToolMaterial {
     }
 
     public static TagKey<Block> level2inverseTag(int level) {
-        switch (level) {
-            case 1:
-                return BlockTags.INCORRECT_FOR_WOODEN_TOOL;
-            case 2:
-                return BlockTags.INCORRECT_FOR_STONE_TOOL;
-            case 3:
-                return BlockTags.INCORRECT_FOR_IRON_TOOL;
-            case 4:
-                return BlockTags.INCORRECT_FOR_GOLD_TOOL;
-            case 5:
-                return BlockTags.INCORRECT_FOR_DIAMOND_TOOL;
-            case 6:
-                return BlockTags.INCORRECT_FOR_NETHERITE_TOOL;
-
-            default:
-                return BlockTags.INCORRECT_FOR_WOODEN_TOOL;
-        }
+        return CompatMiningLevel.fromLevel(level).getInverseTag();
     }
 
     @Deprecated
