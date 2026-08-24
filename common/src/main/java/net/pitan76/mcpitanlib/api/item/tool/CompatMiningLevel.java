a9b9d52ed124b501cd207e615efa61a7db702438
PTOM76 <admin@pitan76.net>
2026-08-24 14:19:51 +0900

    1182

--- common/src/main/java/net/pitan76/mcpitanlib/api/item/tool/CompatMiningLevel.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/item/tool/CompatMiningLevel.java
@@ -1,5 +1,8 @@
 package net.pitan76.mcpitanlib.api.item.tool;
 
+import net.minecraft.block.Block;
+import net.minecraft.tag.TagKey;
+
 /**
  * ツールの採掘レベル。
  * 1.20.4以前はint、1.20.5以降は {@code INCORRECT_FOR_*_TOOL} タグで表現される。
@@ -36,5 +39,9 @@ public enum CompatMiningLevel {
             default:
                 return WOOD;
         }
+    }
+
+    public TagKey<Block> getInverseTag() {
+        return null;
     }
 }
