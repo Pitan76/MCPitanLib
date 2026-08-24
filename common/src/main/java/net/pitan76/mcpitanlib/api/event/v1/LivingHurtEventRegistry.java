b39d2f4ac63b32ab97c0d9aae3ddf6bfea447964
PTOM76 <admin@pitan76.net>
2026-08-24 15:12:10 +0900

    fix: living hurt event, non damage bug

--- common/src/main/java/net/pitan76/mcpitanlib/api/event/v1/LivingHurtEventRegistry.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/event/v1/LivingHurtEventRegistry.java
@@ -16,7 +16,7 @@ public class LivingHurtEventRegistry {
 
         @SuppressWarnings("deprecation")
         default boolean hurt(LivingEntity var1, DamageSource var2, float var3) {
-            return hurt(new LivingHurtEvent(var1, var2, var3)).toActionResult() == InteractionResult.SUCCESS;
+            return hurt(new LivingHurtEvent(var1, var2, var3)).toActionResult() != InteractionResult.FAIL;
         }
 
         net.pitan76.mcpitanlib.api.event.result.EventResult hurt(LivingHurtEvent event);
