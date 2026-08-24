70d92006ab9ff32db0a8f3fc310d4557a84b80e6
PTOM76 <admin@pitan76.net>
2026-08-24 13:51:36 +0900

    [cp] fix1204 stepheight

--- common/src/main/java/net/pitan76/mcpitanlib/api/util/EntityUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/util/EntityUtil.java
@@ -369,9 +369,7 @@ public class EntityUtil {
     }
 
     public static void setStepHeight(Entity entity, float stepHeight) {
-        EntityAttributeInstance instance = getStepHeightAttribute(entity);
-        if (instance == null) return;
-        instance.setBaseValue(stepHeight);
+        entity.setStepHeight(stepHeight);
     }
 
     public static float getStepHeight(Entity entity) {
@@ -379,15 +377,6 @@ public class EntityUtil {
     }
 
     public static float getDefaultStepHeight(Entity entity) {
-        EntityAttributeInstance instance = getStepHeightAttribute(entity);
-        if (instance == null) return 0.0F;
-
-        return (float) instance.getAttribute().value().getDefaultValue();
-    }
-
-    private static EntityAttributeInstance getStepHeightAttribute(Entity entity) {
-        if (!(entity instanceof LivingEntity)) return null;
-
-        return ((LivingEntity) entity).getAttributeInstance(EntityAttributes.STEP_HEIGHT);
+        return 0;
     }
 }
