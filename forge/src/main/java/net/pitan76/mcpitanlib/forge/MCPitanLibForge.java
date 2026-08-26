97abee362ecc1f256a07ea65676f28fd7a34d4a0
PTOM76 <admin@pitan76.net>
2026-08-26 16:43:32 +0900

    [cp] fix BrewingRecipeUtilImpl, PosionBuilder
    # Conflicts:
    #	forge/src/main/java/net/pitan76/mcpitanlib/forge/MCPitanLibForge.java

--- forge/src/main/java/net/pitan76/mcpitanlib/forge/MCPitanLibForge.java
+++ forge/src/main/java/net/pitan76/mcpitanlib/forge/MCPitanLibForge.java
@@ -3,8 +3,11 @@ package net.pitan76.mcpitanlib.forge;
 import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.pitan76.mcpitanlib.MCPitanLib;
+import net.pitan76.mcpitanlib.api.network.forge.ForgeNetworkRegistry;
+import net.pitan76.mcpitanlib.api.potion.BrewingRecipeUtil;
 
 @Mod(MCPitanLib.MOD_ID)
 @SuppressWarnings("deprecation")
@@ -13,6 +16,9 @@ public class MCPitanLibForge {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         EventBuses.registerModEventBus(MCPitanLib.MOD_ID, bus);
 
+        ForgeNetworkRegistry.init();
         MCPitanLib.init();
+
+        bus.addListener((FMLCommonSetupEvent event) -> event.enqueueWork(BrewingRecipeUtil::executeDeferredRecipes));
     }
 }
