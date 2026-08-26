5a5a8ae945028470fb75c0319fb2e4c57590c079
PTOM76 <admin@pitan76.net>
2026-08-26 16:43:32 +0900

    [cp] fix BrewingRecipeUtilImpl, PosionBuilder

--- common/src/main/java/net/pitan76/mcpitanlib/api/potion/BrewingRecipeUtil.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/potion/BrewingRecipeUtil.java
@@ -1,39 +1,46 @@
 package net.pitan76.mcpitanlib.api.potion;
 
+import dev.architectury.injectables.annotations.ExpectPlatform;
 import net.minecraft.item.Item;
 import net.minecraft.potion.Potion;
 import net.pitan76.mcpitanlib.api.registry.result.RegistryResult;
 import net.pitan76.mcpitanlib.midohra.item.ItemWrapper;
 import net.pitan76.mcpitanlib.midohra.potion.PotionWrapper;
-import net.pitan76.mcpitanlib.mixin.BrewingRecipeRegistryInvoker;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 醸造台のレシピを登録する。
  * <p>
- * このバージョンのバニラは登録メソッドがprivateなので、Mixinのinvoker経由で呼ぶ。
+ * このバージョンのバニラは登録メソッドがprivateなので、プラットフォームごとの入口を使う。
  */
 public class BrewingRecipeUtil {
+
+    private static final List<Runnable> deferredRecipes = new ArrayList<>();
 
     /**
      * 水入り瓶などのベースに材料を加えてポーションを作るレシピ。
      */
+    @ExpectPlatform
     public static void registerPotionRecipe(Potion input, Item ingredient, Potion output) {
-        BrewingRecipeRegistryInvoker.registerPotionRecipe_invoke(input, ingredient, output);
+        throw new AssertionError();
     }
 
     public static void registerPotionRecipe(PotionWrapper input, ItemWrapper ingredient, PotionWrapper output) {
-        registerPotionRecipe(input.get(), ingredient.get(), output.get());
+        deferredRecipes.add(() -> registerPotionRecipe(input.get(), ingredient.get(), output.get()));
     }
 
     public static void registerPotionRecipe(RegistryResult<Potion> input, ItemWrapper ingredient, RegistryResult<Potion> output) {
-        registerPotionRecipe(input.get(), ingredient.get(), output.get());
+        deferredRecipes.add(() -> registerPotionRecipe(input.get(), ingredient.get(), output.get()));
     }
 
     /**
      * ポーションの器そのものを変えるレシピ (水入り瓶 → スプラッシュ等)。
      */
+    @ExpectPlatform
     public static void registerItemRecipe(Item input, Item ingredient, Item output) {
-        BrewingRecipeRegistryInvoker.registerItemRecipe_invoke(input, ingredient, output);
+        throw new AssertionError();
     }
 
     public static void registerItemRecipe(ItemWrapper input, ItemWrapper ingredient, ItemWrapper output) {
@@ -41,9 +48,13 @@ public class BrewingRecipeUtil {
     }
 
     /**
-     * 醸造の入力として使えるアイテムを追加する。
+     * 遅延登録されたレシピを一括実行する。～1.20.4
+     * レジストリが凍結された後に呼び出すこと。
      */
-    public static void registerPotionType(ItemWrapper item) {
-        BrewingRecipeRegistryInvoker.registerPotionType_invoke(item.get());
+    public static void executeDeferredRecipes() {
+        for (Runnable recipe : deferredRecipes) {
+            recipe.run();
+        }
+        deferredRecipes.clear();
     }
 }
