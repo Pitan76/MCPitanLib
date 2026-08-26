159ef5047bcddcbb8079c19068f804cb65188c35
PTOM76 <admin@pitan76.net>
2026-08-26 16:46:02 +0900

    [cp] BrewingRecipeUtilImpl

--- forge/src/main/java/net/pitan76/mcpitanlib/api/potion/forge/BrewingRecipeUtilImpl.java
+++ forge/src/main/java/net/pitan76/mcpitanlib/api/potion/forge/BrewingRecipeUtilImpl.java
@@ -4,7 +4,10 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionUtil;
 import net.minecraft.recipe.Ingredient;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+import net.minecraftforge.common.crafting.StrictNBTIngredient;
 
 /**
  * このプラットフォームはItemStack同士でレシピを組むため、ポーションのStackに変換して登録する。
@@ -12,13 +15,14 @@ import net.minecraft.recipe.Ingredient;
 public class BrewingRecipeUtilImpl {
 
     public static void registerPotionRecipe(Potion input, Item ingredient, Potion output) {
-        net.minecraftforge.common.brewing.BrewingRecipeRegistry.addRecipe(
-                Ingredient.ofStacks(net.minecraft.potion.PotionUtil.setPotion(new ItemStack(Items.POTION), input)),
-                Ingredient.ofItems(ingredient),
-                net.minecraft.potion.PotionUtil.setPotion(new ItemStack(Items.POTION), output));
+        ItemStack inputStack = PotionUtil.setPotion(new ItemStack(Items.POTION), input);
+        ItemStack outputStack = PotionUtil.setPotion(new ItemStack(Items.POTION), output);
+
+        BrewingRecipeRegistry.addRecipe(StrictNBTIngredient.of(inputStack),
+                Ingredient.ofItems(ingredient), outputStack);
     }
 
     public static void registerItemRecipe(Item input, Item ingredient, Item output) {
-        net.minecraftforge.common.brewing.BrewingRecipeRegistry.addRecipe(Ingredient.ofItems(input), Ingredient.ofItems(ingredient), new ItemStack(output));
+        BrewingRecipeRegistry.addRecipe(Ingredient.ofItems(input), Ingredient.ofItems(ingredient), new ItemStack(output));
     }
 }
