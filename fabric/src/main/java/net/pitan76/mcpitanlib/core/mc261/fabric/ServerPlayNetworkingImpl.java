d833be1e2636df2971df35ced746cb35f7c467e5
PTOM76 <admin@pitan76.net>
2026-08-27 10:53:28 +0900

    [cp] fix: fix server connect error (network) #196

--- fabric/src/main/java/net/pitan76/mcpitanlib/core/mc261/fabric/ServerPlayNetworkingImpl.java
+++ fabric/src/main/java/net/pitan76/mcpitanlib/core/mc261/fabric/ServerPlayNetworkingImpl.java
@@ -1,7 +1,6 @@
 package net.pitan76.mcpitanlib.core.mc261.fabric;
 
 import io.netty.buffer.Unpooled;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
@@ -9,9 +8,6 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerPlayer;
 import net.pitan76.mcpitanlib.api.network.ServerNetworking;
 import net.pitan76.mcpitanlib.core.network.BufPayload;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ServerPlayNetworkingImpl {
 
@@ -27,13 +23,7 @@ public class ServerPlayNetworkingImpl {
         });
     }
 
-    private static final List<Identifier> registeredList = new ArrayList<>();
-
     public static void registerS2CPayloadType(Identifier identifier) {
-        if (registeredList.contains(identifier)) return;
-        registeredList.add(identifier);
-
-        BufPayload.Type<BufPayload> id = BufPayload.id(identifier);
-        PayloadTypeRegistry.serverboundPlay().register(id, BufPayload.getCodec(id));
+        PayloadTypeRegistryUtil.registerBoth(identifier);
     }
 }
