a6716dd948cebcb9f50bed2b928dd57059e9eee4
PTOM76 <admin@pitan76.net>
2026-09-22 22:52:07 +0900

    fix: crash on dedicated server by loading client-only event handlers

--- forge/src/main/java/net/pitan76/mcpitanlib/forge/MCPitanLibForge.java
+++ forge/src/main/java/net/pitan76/mcpitanlib/forge/MCPitanLibForge.java
@@ -1,6 +1,5 @@
 package net.pitan76.mcpitanlib.forge;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -12,24 +11,10 @@ import net.pitan76.mcpitanlib.api.potion.BrewingRecipeUtil;
 @Mod(MCPitanLib.MOD_ID)
 public class MCPitanLibForge {
     public MCPitanLibForge() {
+        // All event handlers are registered automatically by @EventBusSubscriber.
+        // Do not register them manually here: client-only classes (e.g. CompatRegistryClientImpl)
+        // would then be loaded on dedicated servers and crash with NoClassDefFoundError.
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        bus.register(net.pitan76.mcpitanlib.core.registry.forge.RegistryImpl.class);
-        bus.register(net.pitan76.mcpitanlib.core.registry.forge.CreativeTabEventRegistryImpl.class);
-        bus.register(net.pitan76.mcpitanlib.api.client.registry.forge.CompatRegistryClientImpl.class);
-        bus.register(net.pitan76.mcpitanlib.api.client.registry.forge.KeybindingRegistryImpl.class);
-
-        MinecraftForge.EVENT_BUS.register(net.pitan76.mcpitanlib.core.registry.forge.FuelRegistryImpl.class);
-        MinecraftForge.EVENT_BUS.register(net.pitan76.mcpitanlib.api.client.event.forge.ItemTooltipRegistryImpl.class);
-        MinecraftForge.EVENT_BUS.register(net.pitan76.mcpitanlib.api.command.forge.CommandRegistryImpl.class);
-        MinecraftForge.EVENT_BUS.register(net.pitan76.mcpitanlib.api.event.v0.event.forge.ServerConnectionEventImpl.class);
-        MinecraftForge.EVENT_BUS.register(net.pitan76.mcpitanlib.api.event.v0.forge.AttackEntityEventRegistryImpl.class);
-        MinecraftForge.EVENT_BUS.register(net.pitan76.mcpitanlib.api.event.v0.forge.ClientTickEventRegistryImpl.class);
-        MinecraftForge.EVENT_BUS.register(net.pitan76.mcpitanlib.api.event.v0.forge.EventRegistryServerLifecycleImpl.class);
-        MinecraftForge.EVENT_BUS.register(net.pitan76.mcpitanlib.api.event.v0.forge.InteractionEventRegistryImpl.class);
-        MinecraftForge.EVENT_BUS.register(net.pitan76.mcpitanlib.api.event.v0.forge.LivingHurtEventRegistryImpl.class);
-        MinecraftForge.EVENT_BUS.register(net.pitan76.mcpitanlib.api.event.v1.forge.AttackEntityEventRegistryImpl.class);
-        MinecraftForge.EVENT_BUS.register(net.pitan76.mcpitanlib.api.event.v1.forge.LivingHurtEventRegistryImpl.class);
 
         ForgeNetworkRegistry.init();
         MCPitanLib.init();
