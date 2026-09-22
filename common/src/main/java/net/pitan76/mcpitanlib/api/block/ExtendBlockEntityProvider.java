846aa9f0bb151a4530235a051bff3553f9a9d7cc
PTOM76 <admin@pitan76.net>
2026-09-22 16:51:33 +0900

    central maven repo

--- common/src/main/java/net/pitan76/mcpitanlib/api/block/ExtendBlockEntityProvider.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/block/ExtendBlockEntityProvider.java
@@ -30,6 +30,7 @@ public interface ExtendBlockEntityProvider extends BlockEntityProvider {
      * <pre>{@code
      * public BlockEntity createBlockEntity(TileCreateEvent e) {
      *    return new ExampleBlockEntity(e); // ExampleBlockEntity extends CompatBlockEntity
+     * }
      * }</pre>
      */
     @Nullable
