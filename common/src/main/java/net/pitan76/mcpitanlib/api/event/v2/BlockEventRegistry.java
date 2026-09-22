5e7fe55f4d31254f853c4621117289008e7439a0
PTOM76 <admin@pitan76.net>
2026-09-22 16:51:33 +0900

    [cp] central maven repo

--- common/src/main/java/net/pitan76/mcpitanlib/api/event/v2/BlockEventRegistry.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/event/v2/BlockEventRegistry.java
@@ -9,6 +9,7 @@ public class BlockEventRegistry {
      * <pre>{@code
      * BlockEventRegistry.ON_PLACED.register((event) -> {
      *     // Do something
+     * });
      * }</pre>
      */
     public static OrderedEvent<BlockPlacedTask> ON_PLACED = new OrderedEvent<>();
@@ -19,6 +20,7 @@ public class BlockEventRegistry {
      * BlockEventRegistry.ON_BREAK.register((event) -> {
      *     // Do something
      *     return new BlockBreakResult(event.state);
+     * });
      * }</pre>
      */
     public static OrderedEvent<BlockBreakTask> ON_BREAK = new OrderedEvent<>();
