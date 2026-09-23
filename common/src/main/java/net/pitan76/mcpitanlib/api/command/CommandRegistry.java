977766818629462b9692f5e702f8692e7cc82873
PTOM76 <admin@pitan76.net>
2026-09-23 21:58:44 +0900

    [cp] fix command args require, permission https://github.com/Pitan76/item-alchemy/issues/197

--- common/src/main/java/net/pitan76/mcpitanlib/api/command/CommandRegistry.java
+++ common/src/main/java/net/pitan76/mcpitanlib/api/command/CommandRegistry.java
@@ -33,7 +33,8 @@ public class CommandRegistry {
             // 引数コマンド
             for (Map.Entry<String, ? extends AbstractCommand<?>> argCmd : absCmd.getArgumentCommands().entrySet()) {
                 ArgumentBuilder<CommandSourceStack, ?> nextBuilder = null;
-                argCmd.getValue().init(new CommandSettings());
+                CommandSettings settings = new CommandSettings();
+                argCmd.getValue().init(settings);
 
                 if (argCmd.getValue() instanceof LiteralCommand) {
                     LiteralCommand command = (LiteralCommand) argCmd.getValue();
@@ -98,6 +99,9 @@ public class CommandRegistry {
                                     }
                             );
                 }
+                if (nextBuilder == null) continue;
+
+                nextBuilder.requires(settings::requires);
                 forArgsCmd(argCmd.getValue(), nextBuilder);
                 builder.then(nextBuilder);
             }
