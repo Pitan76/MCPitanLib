e853f10ed92015c33bbd2c27ad2e98159e8402f8
PTOM76 <admin@pitan76.net>
2026-09-23 18:37:55 +0900

    feat: add CompatModelBuilder and CompatArmorModelRegistry for custom (Blockbench) armor models

---
+++ common/src/main/java/net/pitan76/mcpitanlib/api/client/model/CompatCubes.java
@@ -0,0 +1,86 @@
+package net.pitan76.mcpitanlib.api.client.model;
+
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * パーツが持つキューブの一覧 (BlockBench の CubeListBuilder に相当)
+ * <pre>{@code
+ * CompatCubes.create()
+ *     .texOffs(0, 15).addBox(-4, -8, -4, 8, 8, 8)
+ *     .texOffs(0, 0).addBox(-4.5f, -9, -4.5f, 9, 6, 9, 0.5f)
+ * }</pre>
+ */
+public class CompatCubes {
+    protected final List<Cube> cubes = new ArrayList<>();
+    protected int u, v;
+    protected boolean mirror;
+
+    public static CompatCubes create() {
+        return new CompatCubes();
+    }
+
+    public CompatCubes texOffs(int u, int v) {
+        this.u = u;
+        this.v = v;
+        return this;
+    }
+
+    public CompatCubes mirror() {
+        return mirror(true);
+    }
+
+    public CompatCubes mirror(boolean mirror) {
+        this.mirror = mirror;
+        return this;
+    }
+
+    public CompatCubes addBox(float x, float y, float z, float width, float height, float depth) {
+        return addBox(x, y, z, width, height, depth, 0.0F);
+    }
+
+    /**
+     * @param inflate CubeDeformation (キューブを外側に膨らませる量)
+     */
+    public CompatCubes addBox(float x, float y, float z, float width, float height, float depth, float inflate) {
+        cubes.add(new Cube(u, v, x, y, z, width, height, depth, inflate, mirror));
+        return this;
+    }
+
+    public List<Cube> getCubes() {
+        return cubes;
+    }
+
+    public CubeListBuilder toRaw() {
+        CubeListBuilder builder = CubeListBuilder.create();
+        for (Cube cube : cubes) {
+            builder.texOffs(cube.u, cube.v).mirror(cube.mirror)
+                    .addBox(cube.x, cube.y, cube.z, cube.width, cube.height, cube.depth, new CubeDeformation(cube.inflate));
+        }
+        return builder;
+    }
+
+    public static class Cube {
+        public final int u, v;
+        public final float x, y, z;
+        public final float width, height, depth;
+        public final float inflate;
+        public final boolean mirror;
+
+        public Cube(int u, int v, float x, float y, float z, float width, float height, float depth, float inflate, boolean mirror) {
+            this.u = u;
+            this.v = v;
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.width = width;
+            this.height = height;
+            this.depth = depth;
+            this.inflate = inflate;
+            this.mirror = mirror;
+        }
+    }
+}
